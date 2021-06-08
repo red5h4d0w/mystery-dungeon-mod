5a7e9df9813a45aa264d87a4406b3c83c953ce83
red5h4d0w <bussiereismael@gmail.com>
2021-06-07 20:46:13 -0400

    le defend ne vise pas un ennemi

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleDefend.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleDefend.java
@@ -29,7 +29,7 @@ public class SquirtleDefend extends PokemonCard {
     // STAT DECLARATION
 
     private static final CardRarity RARITY = CardRarity.BASIC;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = Pokemon.Enums.SQUIRTLE_BLUE;
 
