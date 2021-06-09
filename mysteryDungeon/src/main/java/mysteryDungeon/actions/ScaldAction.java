a6ea9ed8fdfd67a4ccdec0983b6bd7068c49753d
Ivel54 <abudjo55@gmail.com>
2021-06-09 15:06:04 -0400

    Scald

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/actions/ScaldAction.java
@@ -0,0 +1,72 @@
+package mysteryDungeon.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import mysteryDungeon.powers.BurnPower;
+
+public class ScaldAction extends AbstractGameAction {
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("DiscarAction");
+    public static final String[] TEXT = uiStrings.TEXT;
+    private DamageInfo info;
+    private AbstractPlayer p;
+
+    private int numberOfDiscardedCards = 0;
+    private int burnAmount;
+    private boolean menuPoppedUp = false;
+    public Logger logger = LogManager.getLogger(ScaldAction.class);
+  
+    public ScaldAction(AbstractCreature target, DamageInfo info, int amount, int burnAmount) {
+        this.p = AbstractDungeon.player;
+        this.info = info;
+        this.burnAmount = burnAmount;
+        setValues(target, info);
+        this.amount = amount;
+        this.actionType = AbstractGameAction.ActionType.DAMAGE;
+    }
+  
+    public void update() {
+        if (!menuPoppedUp) {
+            if (p.hand.size() == 0) {
+                this.isDone = true;
+            }
+            else
+            {
+                logger.info(amount);
+                AbstractDungeon.handCardSelectScreen.open(TEXT[0], amount, true, true);
+                tickDuration();
+                menuPoppedUp = true;
+                return;
+            }
+              
+        } 
+        if (!AbstractDungeon.handCardSelectScreen.wereCardsRetrieved) {
+            for (AbstractCard c : AbstractDungeon.handCardSelectScreen.selectedCards.group)
+            {
+                p.hand.moveToDiscardPile(c);
+                numberOfDiscardedCards++; 
+            }
+            AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = true;
+        }
+        if(isDone || AbstractDungeon.handCardSelectScreen.wereCardsRetrieved)
+        {
+            for(int i=0;i<numberOfDiscardedCards;i++)
+            {
+                addToBot(new DamageAction(target, info));
+                addToBot(new ApplyPowerAction(target, info.owner, new BurnPower(target, burnAmount)));
+            }
+            isDone = true;
+        }
+    }
+}
---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleScald.java
@@ -0,0 +1,78 @@
+package mysteryDungeon.cards.Squirtle;
+
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.actions.ScaldAction;
+import mysteryDungeon.cards.PokemonCard;
+import mysteryDungeon.characters.Pokemon;
+import mysteryDungeon.powers.BurnPower;
+
+public class SquirtleScald extends PokemonCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(SquirtleScald.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath("SquirtleAttack.png");
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = Pokemon.Enums.SQUIRTLE_BLUE;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 3;
+    private static final int BASE_MAGIC_NUMBER = 3;
+    private static final int SECOND_MAGIC_NUMBER = 3;
+    private static final int UPGRADE_SECONDMAGIC_NUMBER = 1;
+
+
+    // /STAT DECLARATION/
+
+    public SquirtleScald() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
+        baseSecondMagicNumber = SECOND_MAGIC_NUMBER;
+        secondMagicNumber = baseSecondMagicNumber;
+
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if(upgraded)
+        {
+            addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn)));
+            addToBot(new ApplyPowerAction(m, p, new BurnPower(p, magicNumber), magicNumber));
+        }
+        addToBot(new ScaldAction(m, new DamageInfo(p, damage, damageTypeForTurn), secondMagicNumber, 3)); 
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_SECONDMAGIC_NUMBER);
+            initializeDescription();
+        }
+    }
+}
