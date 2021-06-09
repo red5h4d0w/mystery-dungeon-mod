1f8f2b8d4b22b309a80c9307e73b2d0fa6de52b7
red5h4d0w <bussiereismael@gmail.com>
2021-06-09 18:33:57 -0400

    Update SquirtleRainDish.java

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleRainDish.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleRainDish.java
@@ -35,12 +35,14 @@ public class SquirtleRainDish extends PokemonCard {
     public static final CardColor COLOR = Pokemon.Enums.SQUIRTLE_BLUE;
 
     private static final int COST = 1;
+    private static final int BASE_MAGIC_NUMBER = 1;
 
 
     // /STAT DECLARATION/
 
     public SquirtleRainDish() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = BASE_MAGIC_NUMBER;
         magicNumber = baseMagicNumber;
     }
 
