a5adef6e8da5eabae7b18d2288ffc964cd2cda39
red5h4d0w <bussiereismael@gmail.com>
2021-06-11 17:57:12 -0400

    new tempCards folder + Skull Bash

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtlePrepareSkullBash.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtlePrepareSkullBash.java
@@ -2,10 +2,7 @@ package mysteryDungeon.cards.Squirtle;
 
 import static mysteryDungeon.MysteryDungeon.makeCardPath;
 
-import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.utility.ScryAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
@@ -13,15 +10,16 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import mysteryDungeon.MysteryDungeon;
 import mysteryDungeon.cards.PokemonCard;
+import mysteryDungeon.cards.tempCards.SquirtleSkullBash;
 import mysteryDungeon.characters.Pokemon;
 
-public class SquirtleWaterGun extends PokemonCard {
+public class SquirtlePrepareSkullBash extends PokemonCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = MysteryDungeon.makeID(SquirtleWaterGun.class.getSimpleName());
+    public static final String ID = MysteryDungeon.makeID(SquirtlePrepareSkullBash.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("SquirtleAttack.png");
+    public static final String IMG = makeCardPath("SquirtleSkill.png");
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 
@@ -30,31 +28,26 @@ public class SquirtleWaterGun extends PokemonCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.BASIC;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
-    private static final CardType TYPE = CardType.ATTACK;
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = Pokemon.Enums.SQUIRTLE_BLUE;
 
-    private static final int COST = 1;
-    private static final int UPGRADE_COST = 0;
-    private static final int BASE_DAMAGE = 8;
-    private static final int BASE_MAGIC_NUMBER = 2;
+    private static final int COST = 2;
+    private static final int UPGRADE_COST = 1;
 
 
     // /STAT DECLARATION/
 
-    public SquirtleWaterGun() {
+    public SquirtlePrepareSkullBash() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-        baseDamage = BASE_DAMAGE;
-        baseMagicNumber = BASE_MAGIC_NUMBER;
-        magicNumber = baseMagicNumber;
+        exhaust = true;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AttackEffect.SMASH));
-        addToBot(new ScryAction(magicNumber));
+        addToBot(new MakeTempCardInDrawPileAction(new SquirtleSkullBash(), 1, false, false, false));
     }
 
     // Upgraded stats.
--- mysteryDungeon/src/main/java/mysteryDungeon/cards/tempCards/SquirtleSkullBash.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/tempCards/SquirtleSkullBash.java
@@ -1,25 +1,27 @@
-package mysteryDungeon.cards.Squirtle;
+package mysteryDungeon.cards.tempCards;
 
 import static mysteryDungeon.MysteryDungeon.makeCardPath;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.utility.ScryAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.WeightyImpactEffect;
 
 import mysteryDungeon.MysteryDungeon;
 import mysteryDungeon.cards.PokemonCard;
-import mysteryDungeon.characters.Pokemon;
 
-public class SquirtleWaterGun extends PokemonCard {
+public class SquirtleSkullBash extends PokemonCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = MysteryDungeon.makeID(SquirtleWaterGun.class.getSimpleName());
+    public static final String ID = MysteryDungeon.makeID(SquirtleSkullBash.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String IMG = makeCardPath("SquirtleAttack.png");
     public static final String NAME = cardStrings.NAME;
@@ -30,31 +32,38 @@ public class SquirtleWaterGun extends PokemonCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.BASIC;
+    private static final CardRarity RARITY = CardRarity.SPECIAL;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
-    public static final CardColor COLOR = Pokemon.Enums.SQUIRTLE_BLUE;
+    public static final CardColor COLOR = CardColor.COLORLESS;
 
-    private static final int COST = 1;
-    private static final int UPGRADE_COST = 0;
-    private static final int BASE_DAMAGE = 8;
-    private static final int BASE_MAGIC_NUMBER = 2;
+    private static final int COST = 0;
+    private static final int BASE_DAMAGE = 50;
+    private static final int UPGRADE_PLUS_DMG = 10;
 
 
     // /STAT DECLARATION/
 
-    public SquirtleWaterGun() {
+    public SquirtleSkullBash() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
         baseDamage = BASE_DAMAGE;
-        baseMagicNumber = BASE_MAGIC_NUMBER;
-        magicNumber = baseMagicNumber;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new VFXAction(new WeightyImpactEffect(m.hb.cX, m.hb.cY))); 
         addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AttackEffect.SMASH));
-        addToBot(new ScryAction(magicNumber));
+    }
+
+    @Override
+    public boolean canPlay(AbstractCard card)
+    {
+        if(AbstractDungeon.player.hand.size()==1)
+        {
+            return true;
+        }
+        return false;
     }
 
     // Upgraded stats.
@@ -62,7 +71,7 @@ public class SquirtleWaterGun extends PokemonCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeBaseCost(UPGRADE_COST);
+            upgradeDamage(UPGRADE_PLUS_DMG);
             initializeDescription();
         }
     }
