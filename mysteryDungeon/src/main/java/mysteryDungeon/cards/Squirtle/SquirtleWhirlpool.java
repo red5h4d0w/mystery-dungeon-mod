7e4bb90c25d6ca110046a7e65fce6ebf0ea049aa
Ivel54 <abudjo55@gmail.com>
2021-06-10 22:42:48 -0400

    whirlpool incomplete

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleWhirlpool.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleWhirlpool.java
@@ -2,25 +2,31 @@ package mysteryDungeon.cards.Squirtle;
 
 import static mysteryDungeon.MysteryDungeon.makeCardPath;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.EmptyDeckShuffleAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.actions.common.ShuffleAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 
 import mysteryDungeon.MysteryDungeon;
 import mysteryDungeon.cards.PokemonCard;
 import mysteryDungeon.characters.Pokemon;
+import mysteryDungeon.powers.WhirlpoolPower;
 
-public class SquirtleWithdraw extends PokemonCard {
+public class SquirtleWhirlpool extends PokemonCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = MysteryDungeon.makeID(SquirtleWithdraw.class.getSimpleName());
+    public static final String ID = MysteryDungeon.makeID(SquirtleWhirlpool.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String IMG = makeCardPath("SquirtleSkill.png");
     public static final String NAME = cardStrings.NAME;
@@ -37,37 +43,36 @@ public class SquirtleWithdraw extends PokemonCard {
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = Pokemon.Enums.SQUIRTLE_BLUE;
 
-    private static final int COST = 1;
-    private static final int BASE_BLOCK = 2;
-    private static final int BASE_MAGIC_NUMBER = 1;
-    private static final int UPGRADE_MAGIC_NUMBER = 1;
+    private static final int COST = -1;
+
 
 
     // /STAT DECLARATION/
 
-    public SquirtleWithdraw() {
+    public SquirtleWhirlpool() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-        baseMagicNumber = BASE_MAGIC_NUMBER;
-        magicNumber = baseMagicNumber;
-        baseBlock = BASE_BLOCK;
+        exhaust = true;
     }
 
     // Actions the card should do.
+    // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        if (AbstractDungeon.player.discardPile.size() > 0) {
-            addToBot(new EmptyDeckShuffleAction());
-            addToBot(new ShuffleAction(AbstractDungeon.player.drawPile, false));
+        // Create an int which equals to your current energy.
+        if (energyOnUse < EnergyPanel.totalCount)
+        {
+            energyOnUse = EnergyPanel.totalCount; 
+        }
+        if (p.hasRelic("Chemical X")) {
+            this.energyOnUse += 2;
+            p.getRelic("Chemical X").flash();
         } 
-        if(upgraded)
+        for (int i = 0; i < this.energyOnUse; i++) 
         {
-            addToBot(new DrawCardAction(magicNumber));
-        }
-        for(int i=0;i<p.hand.group.size()-1;i++)
-        {
-            addToBot(new GainBlockAction(p, p, block));
-        }
+            addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)p, (AbstractCreature)p, (AbstractPower)new WhirlpoolPower((AbstractCreature)p, 1), 1));
+        if (!this.freeToPlayOnce)
         
+            p.energy.use(EnergyPanel.totalCount);
     }
 
     // Upgraded stats.
@@ -75,7 +80,6 @@ public class SquirtleWithdraw extends PokemonCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
             rawDescription = UPGRADE_DESCRIPTION;
             initializeDescription();
         }
---
+++ mysteryDungeon/src/main/java/mysteryDungeon/powers/WhirlpoolPower.java
@@ -0,0 +1,70 @@
+package mysteryDungeon.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.util.TextureLoader;
+
+import static mysteryDungeon.MysteryDungeon.makePowerPath;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+
+//Gain 1 dex for the turn for each card played.
+
+public class WhirlpoolPower extends MysteryDungeonPower implements CloneablePowerInterface {
+    public AbstractCreature source;
+
+    public static final String POWER_ID = MysteryDungeon.makeID("WhirlpoolPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
+
+    public WhirlpoolPower(final AbstractCreature owner, final int amount) {
+        name = NAME;
+        ID = POWER_ID;
+
+        this.owner = owner;
+        this.amount = amount;
+
+        type = PowerType.BUFF;
+
+        // We load those txtures here.
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new WhirlpoolPower(owner, amount);
+    }
+
+    public void updateDescription() {
+        if (this.amount == 2) {
+          this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1] + this.amount + DESCRIPTIONS[2];
+        } else {
+          this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[3] + this.amount + DESCRIPTIONS[4];
+        } 
+      }
+      
+      public void atStartOfTurnPostDraw() {
+        flash();
+        addToBot((AbstractGameAction)new DrawCardAction(this.owner, this.amount));
+        addToBot((AbstractGameAction)new DiscardAction(this.owner, this.owner, this.amount, false));
+    }
+}
