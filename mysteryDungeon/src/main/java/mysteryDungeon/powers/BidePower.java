afb9f9a614bb48e531b000d709d8f5629e0965b2
red5h4d0w <bussiereismael@gmail.com>
2021-06-13 14:22:56 -0400

    bide

--- mysteryDungeon/src/main/java/mysteryDungeon/powers/BidePower.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/powers/BidePower.java
@@ -8,21 +8,24 @@ import static mysteryDungeon.MysteryDungeon.makePowerPath;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.math.MathUtils;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.LoseStrengthPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
 
 
 //Gain 1 dex for the turn for each card played.
 
-public class BurnPower extends MysteryDungeonPower implements CloneablePowerInterface {
+public class BidePower extends MysteryDungeonPower implements CloneablePowerInterface {
     public AbstractCreature source;
 
-    public static final String POWER_ID = MysteryDungeon.makeID("BurnPower");
+    public static final String POWER_ID = MysteryDungeon.makeID("BidePower");
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
@@ -33,14 +36,14 @@ public class BurnPower extends MysteryDungeonPower implements CloneablePowerInte
     private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
     private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
 
-    public BurnPower(final AbstractCreature owner, final int amount) {
+    public BidePower(final AbstractCreature owner, final int amount) {
         name = NAME;
         ID = POWER_ID;
 
         this.owner = owner;
         this.amount = amount;
 
-        type = PowerType.DEBUFF;
+        type = PowerType.BUFF;
         isTurnBased = true;
 
         // We load those txtures here.
@@ -51,29 +54,36 @@ public class BurnPower extends MysteryDungeonPower implements CloneablePowerInte
     }
 
     @Override
-    public float atDamageGive(float damage, DamageInfo.DamageType type) {
-        if (type == DamageInfo.DamageType.NORMAL)
-          return damage - MathUtils.floor(this.amount/5.0f); 
-        return damage;
-      }
+    public int onAttacked(DamageInfo info, int damageAmount)
+    {
+        if(info.type == DamageType.NORMAL)
+        {
+            addToBot(new ApplyPowerAction(owner, owner, new StrengthPower(owner, amount), amount));
+            addToBot(new ApplyPowerAction(owner, owner, new LoseStrengthPower(owner, amount), amount));
+        }
+            
+        return damageAmount;
+    }
 
     @Override
-    public void atEndOfRound()
-    {
-        amount-=2;
-        if(amount<=0)
-        {
-            addToBot(new RemoveSpecificPowerAction(owner, owner, this));
-        }
+    public void atEndOfRound() {
+        addToBot(new RemoveSpecificPowerAction(owner, owner, this));
     }
 
     @Override
     public AbstractPower makeCopy() {
-        return new BurnPower(owner, amount);
+        return new BidePower(owner, amount);
     }
 
     @Override
     public void updateDescription() {
-        description = String.format(DESCRIPTIONS[0], MathUtils.floor(amount/5.0f));
+        if(amount == 1)
+        {
+            description = String.format(DESCRIPTIONS[0], amount);
+        } 
+        else
+        {
+            description = String.format(DESCRIPTIONS[1], amount);
+        }
     }
 }
