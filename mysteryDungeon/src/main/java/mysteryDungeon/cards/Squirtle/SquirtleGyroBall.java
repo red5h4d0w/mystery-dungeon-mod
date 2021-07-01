256ed803a71824e082997fd394ba7e620ee5b7aa
red5h4d0w <bussiereismael@gmail.com>
2021-06-30 23:49:19 -0400

    haze gyro ball

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleGyroBall.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleGyroBall.java
@@ -2,26 +2,25 @@ package mysteryDungeon.cards.Squirtle;
 
 import static mysteryDungeon.MysteryDungeon.makeCardPath;
 
-import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.actions.common.DiscardSpecificCardAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-
 import mysteryDungeon.MysteryDungeon;
 import mysteryDungeon.cards.PokemonCard;
-import mysteryDungeon.cards.tempCards.SquirtleFlinch;
 import mysteryDungeon.characters.Pokemon;
 
-public class SquirtleZenHeadbutt extends PokemonCard {
+public class SquirtleGyroBall extends PokemonCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = MysteryDungeon.makeID(SquirtleZenHeadbutt.class.getSimpleName());
+    public static final String ID = MysteryDungeon.makeID(SquirtleGyroBall.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String IMG = makeCardPath("SquirtleAttack.png");
     public static final String NAME = cardStrings.NAME;
@@ -33,37 +32,37 @@ public class SquirtleZenHeadbutt extends PokemonCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = Pokemon.Enums.SQUIRTLE_BLUE;
 
-    private static final int COST = 0;
-    private static final int DAMAGE = 5;
-    private static final int UPGRADE_PLUS_DMG = 2;
+    private static final int COST = 1;
+    private static final int DAMAGE = 6;
+    private static final int UPGRADE_PLUS_DMG = 3;
+    private static final int BASE_MAGIC_NUMBER = 3;
 
 
     // /STAT DECLARATION/
 
-    public SquirtleZenHeadbutt() {
+    public SquirtleGyroBall() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
         baseDamage = DAMAGE;
-        cardsToPreview = new SquirtleFlinch();
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AttackEffect.SMASH));
-        if(!upgraded)
-            addToBot(new MakeTempCardInDrawPileAction(new SquirtleFlinch(), 1, false, false, false));
-        else
-        {
-            AbstractCard upgradedFlinch = new SquirtleFlinch();
-            upgradedFlinch.upgrade();
-            addToBot(new MakeTempCardInDrawPileAction(upgradedFlinch, 1, false, false, false));
+        for (AbstractCard card : p.hand.group) {
+            if(card.type==CardType.STATUS)
+            {
+                addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageType.NORMAL)));
+                addToBot(new DiscardSpecificCardAction(card));
+            }
         }
-            
+        addToBot(new DrawCardAction(p, magicNumber));
     }
 
     // Upgraded stats.
@@ -72,7 +71,6 @@ public class SquirtleZenHeadbutt extends PokemonCard {
         if (!upgraded) {
             upgradeName();
             upgradeDamage(UPGRADE_PLUS_DMG);
-            rawDescription = UPGRADE_DESCRIPTION;
             initializeDescription();
         }
     }
--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleHaze.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleHaze.java
@@ -2,28 +2,28 @@ package mysteryDungeon.cards.Squirtle;
 
 import static mysteryDungeon.MysteryDungeon.makeCardPath;
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.ArtifactPower;
 
 import mysteryDungeon.MysteryDungeon;
 import mysteryDungeon.cards.PokemonCard;
+import mysteryDungeon.cards.Status.StatusFreeze;
 import mysteryDungeon.characters.Pokemon;
 
-public class SquirtleMist extends PokemonCard {
+public class SquirtleHaze extends PokemonCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = MysteryDungeon.makeID(SquirtleMist.class.getSimpleName());
+    public static final String ID = MysteryDungeon.makeID(SquirtleHaze.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String IMG = makeCardPath("SquirtleSkill.png");
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
 
     // /TEXT DECLARATION/
 
@@ -31,43 +31,32 @@ public class SquirtleMist extends PokemonCard {
     // STAT DECLARATION
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = Pokemon.Enums.SQUIRTLE_BLUE;
 
-    private static final int COST = 2;
-    private static final int BLOCK = 12;
-    private static final int BASE_MAGIC_NUMBER = 1;
-    private static final int UPGRADE_MAGIC_NUMBER = 1;
+    private static final int COST = 0;
+    private static final int BASE_MAGIC_NUMBER = 2;
+    private static final int BASE_SECOND_MAGIC_NUMBER = 2;
+    private static final int UPGRADE_SECOND_MAGIC_NUMBER = 1;
 
 
     // /STAT DECLARATION/
 
-    public SquirtleMist() {
+    public SquirtleHaze() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-        baseBlock = BLOCK;
         baseMagicNumber = BASE_MAGIC_NUMBER;
         magicNumber = baseMagicNumber;
+        baseSecondMagicNumber = BASE_SECOND_MAGIC_NUMBER;
+        secondMagicNumber = baseSecondMagicNumber;
+        cardsToPreview = new StatusFreeze();
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        if(!(m.getIntentBaseDmg()>=0))
-            addToBot(new ApplyPowerAction(p, p, new ArtifactPower(p, magicNumber), magicNumber));
-        addToBot(new GainBlockAction(p, p, block));
-    }
-
-    // Check and change the glow of the card if there's an enemy attacking
-    @Override
-    public void triggerOnGlowCheck() {
-        this.glowColor = BLUE_BORDER_GLOW_COLOR.cpy();
-        for (AbstractMonster m : (AbstractDungeon.getCurrRoom()).monsters.monsters) {
-            if (!m.isDeadOrEscaped() && !(m.getIntentBaseDmg() >= 0)) {
-                this.glowColor = GOLD_BORDER_GLOW_COLOR.cpy();
-                break;
-            } 
-        } 
+        addToBot(new GainEnergyAction(secondMagicNumber));
+        addToBot(new MakeTempCardInDiscardAction(new StatusFreeze(), magicNumber));
     }
 
     // Upgraded stats.
@@ -75,7 +64,8 @@ public class SquirtleMist extends PokemonCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
+            upgradeMagicNumber(UPGRADE_SECOND_MAGIC_NUMBER);
+            rawDescription = UPGRADE_DESCRIPTION;
             initializeDescription();
         }
     }
