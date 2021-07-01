baea398758d3add762034813577c59482dc9c8df
red5h4d0w <bussiereismael@gmail.com>
2021-06-30 23:22:48 -0400

    fix

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleMist.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleMist.java
@@ -30,12 +30,12 @@ public class SquirtleMist extends PokemonCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = Pokemon.Enums.SQUIRTLE_BLUE;
 
-    private static final int COST = 1;
+    private static final int COST = 2;
     private static final int BLOCK = 12;
     private static final int BASE_MAGIC_NUMBER = 1;
     private static final int UPGRADE_MAGIC_NUMBER = 1;
