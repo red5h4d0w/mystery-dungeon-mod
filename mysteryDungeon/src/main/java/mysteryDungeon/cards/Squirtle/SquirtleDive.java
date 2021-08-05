9eaba4a143facf309143463e03290662711534fa
red5h4d0w <bussiereismael@gmail.com>
2021-08-04 22:01:21 -0400

    fix Dive

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleDive.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleDive.java
@@ -2,6 +2,8 @@ package mysteryDungeon.cards.Squirtle;
 
 import static mysteryDungeon.MysteryDungeon.makeCardPath;
 
+import java.util.ArrayList;
+
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -51,19 +53,29 @@ public class SquirtleDive extends PokemonCard {
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        ArrayList<AbstractCard> drawPileCardsToRemove = new ArrayList<AbstractCard>();
         for(AbstractCard c : p.drawPile.group)
         {
             if(c.type == CardType.STATUS)
             {
-                p.drawPile.moveToExhaustPile(c);
+                drawPileCardsToRemove.add(c);
             }
         }
+        for(AbstractCard c : drawPileCardsToRemove)
+        {
+            p.drawPile.moveToExhaustPile(c);
+        }
+        ArrayList<AbstractCard> discardPileCardsToRemove = new ArrayList<AbstractCard>();
         for(AbstractCard c : p.discardPile.group)
         {
             if(c.type == CardType.STATUS)
             {
-                p.discardPile.moveToExhaustPile(c);
+                discardPileCardsToRemove.add(c);
             }
+        }
+        for(AbstractCard c : discardPileCardsToRemove)
+        {
+            p.drawPile.moveToExhaustPile(c);
         }
         for(AbstractCard c : p.hand.group)
         {
