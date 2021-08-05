243c49a2ed3caaa18270793be079f45cf32d1d72
Ivel54 <abudjo55@gmail.com>
2021-08-04 20:36:40 -0400

    Flip Turn

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleFlipTurn.java
@@ -0,0 +1,71 @@
+package mysteryDungeon.cards.Squirtle;
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.cards.PokemonCard;
+import mysteryDungeon.characters.Pokemon;
+
+
+public class SquirtleFlipTurn extends PokemonCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(SquirtleFlipTurn.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath("SquirtleSkill.png");
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Pokemon.Enums.SQUIRTLE_BLUE;
+
+    private static final int COST = 1;
+    private static final int BLOCK = 7;
+    private static final int UPGRADE_PLUS_BLOCK = 4;
+    private static final int BASE_MAGIC_NUMBER = 1;
+    private static final int UPGRADE_MAGIC_NUMBER = 1;
+
+
+
+    // /STAT DECLARATION/
+
+    public SquirtleFlipTurn() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseBlock = BLOCK;
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, block));
+        AbstractDungeon.actionManager.addToBottom(new DrawCardAction(p, magicNumber));
+        AbstractDungeon.actionManager.addToBottom(new DiscardAction(p, p, magicNumber, false));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_PLUS_BLOCK);
+            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
+            initializeDescription();
+        }
+    }
+}
