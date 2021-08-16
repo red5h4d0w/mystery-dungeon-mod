1b5ad189e258c1c1e20bfaf2e6652474e61755f6
Ivel54 <abudjo55@gmail.com>
2021-08-16 18:01:52 -0400

    double edge pls test

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Bulbasaur/BulbasaurDoubleEdge.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Bulbasaur/BulbasaurDoubleEdge.java
@@ -2,23 +2,32 @@ package mysteryDungeon.cards.Bulbasaur;
 
 import static mysteryDungeon.MysteryDungeon.makeCardPath;
 
-import basemod.abstracts.CustomCard;
+import basemod.abstracts.CustomCard
+;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+import com.megacrit.cardcrawl.vfx.combat.VerticalImpactEffect;
 
 import mysteryDungeon.MysteryDungeon;
 import mysteryDungeon.characters.Pokemon;
 
-public class BulbasaurDoubleEdge extends CustomCard {
+
+public class BulbasaurDoubleEdge extends mysteryDungeon.cards.PokemonCard {
 
     
 
@@ -39,9 +48,10 @@ public class BulbasaurDoubleEdge extends CustomCard {
     public static final CardColor COLOR = Pokemon.Enums.BULBASAUR_GREEN;
 
     private static final int COST = 2;
-    private static final int BASE_DAMAGE = 15;
-    private static final int BASE_MAGIC_NUMBER = 3;
-    private static final int UPGRADE_MAGIC_NUMBER = 3;
+    private static final int BASE_DAMAGE = 9;
+    private static final int BASE_MAGIC_NUMBER = 4;
+    private static final int SECOND_MAGIC_NUMBER = 1;
+    
 
 
     // /STAT DECLARATION/
@@ -51,9 +61,27 @@ public class BulbasaurDoubleEdge extends CustomCard {
         baseDamage = BASE_DAMAGE;
         baseMagicNumber = BASE_MAGIC_NUMBER;
         magicNumber = baseMagicNumber;
+        baseSecondMagicNumber = SECOND_MAGIC_NUMBER;
+        secondMagicNumber = baseSecondMagicNumber;
     }
 
-    public void calculateCardDamage(AbstractMonster mo) {
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (m != null)
+        addToBot((AbstractGameAction)new VFXAction((AbstractGameEffect)new VerticalImpactEffect(m.hb.cX + m.hb.width / 4.0F, m.hb.cY - m.hb.height / 4.0F))); 
+        addToBot((AbstractGameAction)new DamageAction((AbstractCreature)m, new DamageInfo((AbstractCreature)p, this.damage, this.damageTypeForTurn), AttackEffect.BLUNT_HEAVY));
+        addToBot((AbstractGameAction)new ApplyPowerAction(p, p,new DexterityPower(m, -this.secondMagicNumber), -this.secondMagicNumber));
+    }
+
+    public void applyPowers() {
+        AbstractPower strength = AbstractDungeon.player.getPower("Strength");
+        if (strength != null)
+          strength.amount *= this.magicNumber; 
+        super.applyPowers();
+        if (strength != null)
+          strength.amount /= this.magicNumber; 
+      }
+
+      public void calculateCardDamage(AbstractMonster mo) {
         AbstractPower strength = AbstractDungeon.player.getPower("Strength");
         if (strength != null)
           strength.amount *= this.magicNumber; 
@@ -62,25 +90,12 @@ public class BulbasaurDoubleEdge extends CustomCard {
           strength.amount /= this.magicNumber; 
       }
 
-    // Actions the card should do.
-    @Override
-    public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractPower strength = p.getPower("Strength");
-        int recoil = 0;
-        if(strength!=null)
-        {
-            recoil = strength.amount>0?(int)Math.ceil((float)strength.amount/2.0f):0;
-        }
-        addToBot(new DamageAction(m, new DamageInfo(p, damage), AttackEffect.SMASH));
-        addToBot(new DamageAction(p, new DamageInfo(p, recoil, DamageType.THORNS), AttackEffect.SMASH));
-    }
 
     // Upgraded stats.
     @Override
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
             initializeDescription();
         }
     }
