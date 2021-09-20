b2b90ef04f504e184a101f66063b44d60da824c3
Ivel54 <abudjo55@gmail.com>
2021-09-20 13:17:43 -0400

    `Wlid Charge

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Pikachu/PikachuWildCharge.java
@@ -0,0 +1,70 @@
+package mysteryDungeon.cards.Pikachu;
+
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.defect.DecreaseMaxOrbAction;
+import com.megacrit.cardcrawl.actions.defect.EvokeAllOrbsAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.cards.PokemonCard;
+import mysteryDungeon.characters.Pokemon;
+
+public class PikachuWildCharge extends PokemonCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(PikachuWildCharge.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath("PikachuSkill.png");
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Pokemon.Enums.PIKACHU_YELLOW;
+
+    private static final int COST = 2;
+
+
+
+    // /STAT DECLARATION/
+
+    public PikachuWildCharge() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        inert = true;
+        exhaust = true;
+
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToTop(new EvokeAllOrbsAction());
+        addToBot((AbstractGameAction)new DecreaseMaxOrbAction(1));
+
+
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            inert = false;
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
