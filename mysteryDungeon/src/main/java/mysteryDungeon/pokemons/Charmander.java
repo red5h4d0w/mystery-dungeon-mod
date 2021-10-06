3c2e6194b16f05983516cc194ce1541dfe917a9a
Ivel54 <abudjo55@gmail.com>
2021-10-06 10:06:53 -0400

    Ember is back

--- mysteryDungeon/src/main/java/mysteryDungeon/pokemons/Charmander.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/pokemons/Charmander.java
@@ -7,7 +7,7 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.AbstractCard.CardColor;
 
 import mysteryDungeon.cards.Charmander.CharmanderDefend;
-import mysteryDungeon.cards.Charmander.CharmanderFireFang;
+import mysteryDungeon.cards.Charmander.CharmanderEmber;
 import mysteryDungeon.cards.Charmander.CharmanderScratch;
 import mysteryDungeon.characters.Pokemon;
 
@@ -16,7 +16,7 @@ public class Charmander extends AbstractPokemon {
     public static String NAME = "Charmander";
     public static int MAX_HP = 30;
     public static int ORB_SLOTS = 1;
-    public static AbstractCard[] STARTING_DECK = new AbstractCard[]{new CharmanderScratch(), new CharmanderScratch(), new CharmanderDefend(), new CharmanderDefend(), new CharmanderFireFang()};
+    public static AbstractCard[] STARTING_DECK = new AbstractCard[]{new CharmanderScratch(), new CharmanderScratch(), new CharmanderDefend(), new CharmanderDefend(), new CharmanderEmber()};
     public static Color COLOR = Color.RED;
     public static CardColor CARD_COLOR = Pokemon.Enums.CHARMANDER_RED;
     public static String PATH_TO_BACK_SPRITE = makeBackSpritePath(Charmander.class.getSimpleName()+".png");
