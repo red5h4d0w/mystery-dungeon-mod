c08ed7bbcb4cf16348beb7ceb3886a3ea3868252
Ivel54 <abudjo55@gmail.com>
2021-10-11 16:15:17 -0400

    Brave explorer changed

--- mysteryDungeon/src/main/java/mysteryDungeon/relics/BraveExplorerRelic.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/relics/BraveExplorerRelic.java
@@ -8,20 +8,16 @@ import static mysteryDungeon.MysteryDungeon.makeRelicOutlinePath;
 import static mysteryDungeon.MysteryDungeon.makeRelicPath;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.evacipated.cardcrawl.mod.stslib.relics.OnReceivePowerRelic;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.AbstractCard.CardType;
-import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.badlogic.gdx.math.MathUtils;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.PowerTip;
 import com.megacrit.cardcrawl.localization.RelicStrings;
-import com.megacrit.cardcrawl.powers.AbstractPower;
-import com.megacrit.cardcrawl.powers.AbstractPower.PowerType;
+import com.megacrit.cardcrawl.powers.StrengthPower;
 
-public class BraveExplorerRelic extends CustomRelic implements OnReceivePowerRelic{ // You must implement things you want to use from StSlib
+public class BraveExplorerRelic extends CustomRelic { // You must implement things you want to use from StSlib
     /*
      * https://github.com/daviscook477/BaseMod/wiki/Custom-Relics
      * StSLib for Clickable Relics
@@ -38,7 +34,7 @@ public class BraveExplorerRelic extends CustomRelic implements OnReceivePowerRel
     private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("default_clickable_relic.png"));
     private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("default_clickable_relic.png"));
 
-    private boolean used = false; // We should make sure the relic is only activateable during our turn, not the enemies'.
+    public boolean trigger = false;
 
     public BraveExplorerRelic() {
         super(ID, IMG, OUTLINE, RelicTier.STARTER, LandingSound.CLINK);
@@ -47,35 +43,31 @@ public class BraveExplorerRelic extends CustomRelic implements OnReceivePowerRel
         tips.add(new PowerTip(name, description));
     }
 
-    @Override
-    public void onCardDraw(AbstractCard card)
-    {
-        if(card.type == CardType.STATUS && !used)
-        {
-            flash();
-            addToBot(new ExhaustSpecificCardAction(card, AbstractDungeon.player.hand));
-            addToBot(new DrawCardAction(AbstractDungeon.player, 1));
-            used = true;
-        }
-    }
-
-    @Override
-    public boolean onReceivePower(AbstractPower power, AbstractCreature source) {
-        if(power.type==PowerType.DEBUFF && !used)
-        {
-            flash();
-            used = true;
-            return false;
-        }
-        return true;
-    }
-
-
-    @Override
-    public void atPreBattle()
-    {
-        used = false;
-    }
+    public void onPlayerEndTurn() {
+        if (AbstractDungeon.player.currentBlock == 0 || trigger) {
+          trigger = false;
+          flash();
+          stopPulse();
+          addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new StrengthPower(AbstractDungeon.player, 1)));
+          addToTop(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+        } 
+      }
+      
+      public void atTurnStart() {
+        trigger = false;
+        if (AbstractDungeon.player.currentBlock == 0)
+          beginLongPulse(); 
+      }
+      
+      public int onPlayerGainedBlock(float blockAmount) {
+        if (blockAmount > 0.0F)
+          stopPulse(); 
+        return MathUtils.floor(blockAmount);
+      }
+      
+      public void onVictory() {
+        stopPulse();
+      }
 
     // Description
     @Override
