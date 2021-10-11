61e4d752bf492d95ba4e9c8160a6be54a0b2980a
Ivel54 <abudjo55@gmail.com>
2021-10-11 13:57:56 -0400

    Nerf hail

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleHail.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleHail.java
@@ -31,7 +31,7 @@ public class SquirtleHail extends PokemonCard {
     public static final CardColor COLOR = Pokemon.Enums.SQUIRTLE_BLUE;
 
     private static final int COST = 1;
-    private static final int BASE_MAGIC_NUMBER = 3;
+    private static final int BASE_MAGIC_NUMBER = 1;
     private static final int UPGRADE_BASE_MAGIC_NUMBER = 2;
 
 
