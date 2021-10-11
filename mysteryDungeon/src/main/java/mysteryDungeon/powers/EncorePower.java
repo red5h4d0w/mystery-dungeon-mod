30f44d63f84df3638533284bd24304f21f1b880c
red5h4d0w <bussiereismael@gmail.com>
2021-10-11 12:33:48 -0400

    encore peut compiler

--- mysteryDungeon/src/main/java/mysteryDungeon/powers/EncorePower.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/powers/EncorePower.java
@@ -9,17 +9,13 @@ import static mysteryDungeon.MysteryDungeon.makePowerPath;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DamageRandomEnemyAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardQueueItem;
-import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -58,11 +54,11 @@ public class EncorePower extends MysteryDungeonPower implements CloneablePowerIn
 
     
     public void onUseCard(AbstractCard card, UseCardAction action) {
-        if (!card.purgeOnUse && card.type == AbstractCard.CardType.ATTACK && this.amount > 0) {
+        if (!card.purgeOnUse && card.type == AbstractCard.CardType.ATTACK && amount > 0) {
             flash();
             AbstractMonster m = null;
             if (action.target != null)
-              m = (AbstractMonster)action.target; 
+                m = (AbstractMonster)action.target; 
             AbstractCard tmp = card.makeSameInstanceOf();
             AbstractDungeon.player.limbo.addToBottom(tmp);
             tmp.current_x = card.current_x;
@@ -70,18 +66,18 @@ public class EncorePower extends MysteryDungeonPower implements CloneablePowerIn
             tmp.target_x = Settings.WIDTH / 2.0F - 300.0F * Settings.scale;
             tmp.target_y = Settings.HEIGHT / 2.0F;
             if (m != null)
-              tmp.calculateCardDamage(m); 
+                tmp.calculateCardDamage(m); 
             tmp.purgeOnUse = true;
             AbstractDungeon.actionManager.addCardQueueItem(new CardQueueItem(tmp, m, card.energyOnUse, true, true), true);
-            this.amount--;
-            if (this.amount == 0)
-            addToBot((AbstractGameAction)new RemoveSpecificPowerAction(this.owner, this.owner, "Encore"));
+            amount--;
+            if (amount == 0)
+            addToBot(new RemoveSpecificPowerAction(owner, owner, this));
         }
         if (!card.purgeOnUse && card.type == AbstractCard.CardType.SKILL && this.amount > 0) {
             flash();
             AbstractMonster m = null;
             if (action.target != null)
-              m = (AbstractMonster)action.target; 
+                m = (AbstractMonster)action.target; 
             AbstractCard tmp = card.makeSameInstanceOf();
             AbstractDungeon.player.limbo.addToBottom(tmp);
             tmp.current_x = card.current_x;
@@ -89,18 +85,18 @@ public class EncorePower extends MysteryDungeonPower implements CloneablePowerIn
             tmp.target_x = Settings.WIDTH / 2.0F - 300.0F * Settings.scale;
             tmp.target_y = Settings.HEIGHT / 2.0F;
             if (m != null)
-              tmp.calculateCardDamage(m); 
+                tmp.calculateCardDamage(m); 
             tmp.purgeOnUse = true;
             AbstractDungeon.actionManager.addCardQueueItem(new CardQueueItem(tmp, m, card.energyOnUse, true, true), true);
-            this.amount--;
-            if (this.amount == 0)
-              addToTop((AbstractGameAction)new RemoveSpecificPowerAction(this.owner, this.owner, "Encore")); 
+            amount--;
+            if (amount == 0)
+              addToTop(new RemoveSpecificPowerAction(owner, owner, this)); 
           }  
     }
 
     public void atEndOfTurn(boolean isPlayer) {
         if (isPlayer)
-          addToBot((AbstractGameAction)new RemoveSpecificPowerAction(this.owner, this.owner, "Encore"));
+          addToBot(new RemoveSpecificPowerAction(owner, owner, this));
 }
     @Override
     public AbstractPower makeCopy() {
