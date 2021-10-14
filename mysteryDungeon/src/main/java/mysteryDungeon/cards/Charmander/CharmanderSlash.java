c26222b7ba5df2a11cc426e707873222f87cf1bf
Ivel54 <abudjo55@gmail.com>
2021-10-14 02:09:50 -0400

    Slash finished

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Charmander/CharmanderSlash.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Charmander/CharmanderSlash.java
@@ -36,7 +36,7 @@ public class CharmanderSlash extends CustomCard {
     // STAT DECLARATION
 
     private static final CardRarity RARITY = CardRarity.COMMON;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = Pokemon.Enums.CHARMANDER_RED;
 
