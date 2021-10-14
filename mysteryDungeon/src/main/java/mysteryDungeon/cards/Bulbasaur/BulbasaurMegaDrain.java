cb1b36907fddf1588cc8f9701f6c06a70d72a172
Ivel54 <abudjo55@gmail.com>
2021-10-14 02:41:16 -0400

    mega drain nerf

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Bulbasaur/BulbasaurMegaDrain.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Bulbasaur/BulbasaurMegaDrain.java
@@ -36,7 +36,7 @@ public class BulbasaurMegaDrain extends PokemonCard {
     public static final CardColor COLOR = Pokemon.Enums.BULBASAUR_GREEN;
 
     private static final int COST = 1;
-    private static final int BASE_MAGIC_NUMBER = 2;
+    private static final int BASE_MAGIC_NUMBER = 1;
     private static final int UPGRADE_MAGIC_NUMBER = 1;
 
 
