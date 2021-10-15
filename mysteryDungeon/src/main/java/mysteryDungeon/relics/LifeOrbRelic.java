de070907aa9937c1e8058d417ea906b6edb05afb
Ivel54 <abudjo55@gmail.com>
2021-10-15 14:45:24 -0400

    Life Orb Relic

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/relics/LifeOrbRelic.java
@@ -0,0 +1,59 @@
+package mysteryDungeon.relics;
+
+import basemod.abstracts.CustomRelic;
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.util.TextureLoader;
+
+import static mysteryDungeon.MysteryDungeon.makeRelicOutlinePath;
+import static mysteryDungeon.MysteryDungeon.makeRelicPath;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.actions.common.ExhaustAction;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.localization.RelicStrings;
+
+public class LifeOrbRelic extends CustomRelic { // You must implement things you want to use from StSlib
+    /*
+     * https://github.com/daviscook477/BaseMod/wiki/Custom-Relics
+     * StSLib for Clickable Relics
+     *
+     * Usable once per turn. Right click: Evoke your rightmost orb.
+     */
+
+    // ID, images, text.
+    public static final String ID = MysteryDungeon.makeID("LifeOrbRelic");
+
+    private static final RelicStrings relicStrings = CardCrawlGame.languagePack.getRelicStrings(ID);
+    public static final String NAME = relicStrings.NAME;
+    public static final String[] DESCRIPTIONS = relicStrings.DESCRIPTIONS;
+    private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("default_clickable_relic.png"));
+    private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("default_clickable_relic.png"));
+
+    public LifeOrbRelic() {
+        super(ID, IMG, OUTLINE, RelicTier.BOSS, LandingSound.CLINK);
+
+        tips.clear();
+        tips.add(new PowerTip(name, description));
+    }
+
+    @Override
+    public void onEquip() {
+        AbstractDungeon.player.energy.energyMaster++;
+      }
+      
+    public void onUnequip() {
+        AbstractDungeon.player.energy.energyMaster--;
+      }
+
+    public void atEndOfTurn(){
+        addToBot(new ExhaustAction(1, true, false, false));
+    }
+    // Description
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+
+}
