620c12654f3372f1f07ff73d407a7ffc72feebb2
Ivel54 <abudjo55@gmail.com>
2021-10-18 19:27:39 -0400

    Charcoal relic

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/relics/CharcoalRelic.java
@@ -0,0 +1,66 @@
+package mysteryDungeon.relics;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.pokemons.Charmander;
+import mysteryDungeon.util.TextureLoader;
+
+import static mysteryDungeon.MysteryDungeon.makeRelicOutlinePath;
+import static mysteryDungeon.MysteryDungeon.makeRelicPath;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.evacipated.cardcrawl.mod.stslib.relics.OnApplyPowerRelic;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.localization.RelicStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.AbstractPower.PowerType;
+
+public class CharcoalRelic extends AbstractPokemonRelic implements OnApplyPowerRelic { // You must implement things you want to use from StSlib
+    /*
+     * https://github.com/daviscook477/BaseMod/wiki/Custom-Relics
+     * StSLib for Clickable Relics
+     *
+     * Usable once per turn. Right click: Evoke your rightmost orb.
+     */
+
+    // ID, images, text.
+    public static final String ID = MysteryDungeon.makeID("CharcoalRelic");
+
+    private static final RelicStrings relicStrings = CardCrawlGame.languagePack.getRelicStrings(ID);
+    public static final String NAME = relicStrings.NAME;
+    public static final String[] DESCRIPTIONS = relicStrings.DESCRIPTIONS;
+    private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("flame-orb.png"));
+    private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("flame-orb.png"));
+
+    public CharcoalRelic() {
+        super(ID, IMG, OUTLINE, RelicTier.RARE, LandingSound.CLINK);
+
+        color = Charmander.COLOR;
+        tips.clear();
+        tips.add(new PowerTip(name, description));
+    }
+
+    @Override
+    public boolean onApplyPower(AbstractPower power, AbstractCreature target, AbstractCreature source) {
+        if(power.type == PowerType.DEBUFF){
+            addToBot((AbstractGameAction)new RelicAboveCreatureAction((AbstractCreature)AbstractDungeon.player, this));
+            addToBot((AbstractGameAction)new DamageAllEnemiesAction(null, 
+                  
+                  DamageInfo.createDamageMatrix(2, true), DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+        }
+        return true;
+      }
+
+    // Description
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+
+}
