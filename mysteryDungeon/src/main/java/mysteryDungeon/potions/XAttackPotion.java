a0cd9c04e56cf8000a49ee84a1ca4b98f1dad918
Ivel54 <abudjo55@gmail.com>
2021-10-19 15:46:30 -0400

    X Attack no desc

--- mysteryDungeon/src/main/java/mysteryDungeon/potions/XAttackPotion.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/potions/XAttackPotion.java
@@ -1,25 +1,26 @@
 package mysteryDungeon.potions;
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.PowerTip;
 import com.megacrit.cardcrawl.localization.PotionStrings;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
-import com.megacrit.cardcrawl.powers.PoisonPower;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 import basemod.abstracts.CustomPotion;
+import mysteryDungeon.cards.tempCards.SquirtleFlinch;
 
-public class PoisonBarbPotion extends CustomPotion {
+public class XAttackPotion extends CustomPotion {
 
-    public static final String POTION_ID = mysteryDungeon.MysteryDungeon.makeID("PoisonBarbPotion");
+    public static final String POTION_ID = mysteryDungeon.MysteryDungeon.makeID("XAttackPotion");
     private static final PotionStrings potionStrings = CardCrawlGame.languagePack.getPotionString(POTION_ID);
     
     public static final String NAME = potionStrings.NAME;
     public static final String[] DESCRIPTIONS = potionStrings.DESCRIPTIONS;
 
-    public PoisonBarbPotion() {
+    public XAttackPotion() {
         // The bottle shape and inside is determined by potion size and color. The actual colors are the main MysteryDungeon.java
         super(NAME, POTION_ID, PotionRarity.COMMON, PotionSize.M, PotionColor.SMOKE);
         
@@ -30,7 +31,7 @@ public class PoisonBarbPotion extends CustomPotion {
         description = DESCRIPTIONS[0] + potency + DESCRIPTIONS[2] + DESCRIPTIONS[1] + potency + DESCRIPTIONS[2];
         
        // Do you throw this potion at an enemy or do you just consume it.
-        isThrown = true;
+        isThrown = false;
         
         // Initialize the on-hover name + description
         tips.add(new PowerTip(name, description));
@@ -52,23 +53,26 @@ public class PoisonBarbPotion extends CustomPotion {
 
     @Override
     public void use(AbstractCreature target) {
-        addToBot((new ApplyPowerAction(target, (AbstractCreature)AbstractDungeon.player, new PoisonPower(target, (AbstractCreature)AbstractDungeon.player, this.potency), this.potency)));
+        SquirtleFlinch Flinch = new SquirtleFlinch();
+        if ((AbstractDungeon.getCurrRoom()).phase == AbstractRoom.RoomPhase.COMBAT)
+        Flinch.setCostForTurn(0);
+          addToBot(new MakeTempCardInHandAction(Flinch.makeStatEquivalentCopy(), this.potency)); 
       }
     
     @Override
     public AbstractPotion makeCopy() {
-        return new PoisonBarbPotion();
+        return new XAttackPotion();
     }
 
     // This is your potency.
     @Override
     public int getPotency(final int potency) {
-        return 6;
+        return 3;
     }
 
     public void upgradePotion()
     {
-      potency += 6;
+      potency += 1;
       tips.clear();
       tips.add(new PowerTip(name, description));
     }
