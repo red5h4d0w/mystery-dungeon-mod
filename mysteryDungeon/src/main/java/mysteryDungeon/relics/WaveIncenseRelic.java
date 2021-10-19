af8935a324a01cf940de00794a89dae3cce5adf6
Ivel54 <abudjo55@gmail.com>
2021-10-18 22:05:16 -0400

    Wave Incense

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/relics/WaveIncenseRelic.java
@@ -0,0 +1,54 @@
+package mysteryDungeon.relics;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.AbstractPokemonRelic;
+import mysteryDungeon.pokemons.Squirtle;
+import mysteryDungeon.util.TextureLoader;
+
+import static mysteryDungeon.MysteryDungeon.makeRelicOutlinePath;
+import static mysteryDungeon.MysteryDungeon.makeRelicPath;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.localization.RelicStrings;
+import com.megacrit.cardcrawl.powers.watcher.MasterRealityPower;
+
+public class WaveIncenseRelic extends AbstractPokemonRelic { // You must implement things you want to use from StSlib
+    /*
+     * https://github.com/daviscook477/BaseMod/wiki/Custom-Relics
+     * StSLib for Clickable Relics
+     *
+     * Usable once per turn. Right click: Evoke your rightmost orb.
+     */
+
+    // ID, images, text.
+    public static final String ID = MysteryDungeon.makeID(WaveIncenseRelic.class.getSimpleName());
+
+    private static final RelicStrings relicStrings = CardCrawlGame.languagePack.getRelicStrings(ID);
+    public static final String NAME = relicStrings.NAME;
+    public static final String[] DESCRIPTIONS = relicStrings.DESCRIPTIONS;
+    private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("mystic-water.png"));
+    private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("mystic-water.png"));
+
+    public WaveIncenseRelic() {
+        super(ID, IMG, OUTLINE, RelicTier.SHOP, LandingSound.CLINK);
+
+        color = Squirtle.COLOR;
+        tips.clear();
+        tips.add(new PowerTip(name, description));
+    }
+
+    @Override
+    public void atPreBattle()
+    {
+        addToBot(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new MasterRealityPower(AbstractDungeon.player)));
+    }
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+
+}
