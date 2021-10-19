f2b9566d24206b4f412c5680cf2742756598b57a
Ivel54 <abudjo55@gmail.com>
2021-10-19 16:26:00 -0400

    Flame Orb Potion no desc

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/potions/FlameOrbPotion.java
@@ -0,0 +1,81 @@
+package mysteryDungeon.potions;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.localization.PotionStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.potions.AbstractPotion;
+
+import basemod.abstracts.CustomPotion;
+import mysteryDungeon.powers.BurnPower;
+
+public class FlameOrbPotion extends CustomPotion {
+
+    public static final String POTION_ID = mysteryDungeon.MysteryDungeon.makeID("FlameOrbPotion");
+    private static final PotionStrings potionStrings = CardCrawlGame.languagePack.getPotionString(POTION_ID);
+    
+    public static final String NAME = potionStrings.NAME;
+    public static final String[] DESCRIPTIONS = potionStrings.DESCRIPTIONS;
+
+    public FlameOrbPotion() {
+        // The bottle shape and inside is determined by potion size and color. The actual colors are the main MysteryDungeon.java
+        super(NAME, POTION_ID, PotionRarity.UNCOMMON, PotionSize.M, PotionColor.SMOKE);
+        
+        // Potency is the damage/magic number equivalent of potions.
+        potency = getPotency();
+        
+        // Initialize the Description
+        description = DESCRIPTIONS[0] + potency + DESCRIPTIONS[2] + DESCRIPTIONS[1] + potency + DESCRIPTIONS[2];
+        
+       // Do you throw this potion at an enemy or do you just consume it.
+        isThrown = true;
+        
+        // Initialize the on-hover name + description
+        tips.add(new PowerTip(name, description));
+        
+    }
+    // See that description? It has DESCRIPTIONS[1] instead of just hard-coding the "text " + potency + " more text" inside.
+    // DO NOT HARDCODE YOUR STRINGS ANYWHERE, it's really bad practice to have "Strings" in your code:
+
+    /*
+     * 1. It's bad for if somebody likes your mod enough (or if you decide) to translate it.
+     * Having only the JSON files for translation rather than 15 different instances of "Dexterity" in some random cards is A LOT easier.
+     *
+     * 2. You don't have a centralised file for all strings for easy proof-reading. If you ever want to change a string
+     * you don't have to go through all your files individually/pray that a mass-replace doesn't screw something up.
+     *
+     * 3. Without hardcoded strings, editing a string doesn't require a compile, saving you time (unless you clean+package).
+     *
+     */
+
+    @Override
+    public void use(AbstractCreature target) {
+        for (AbstractMonster mo : AbstractDungeon.getMonsters().monsters) {
+          if (!mo.isDeadOrEscaped())
+          addToBot(new ApplyPowerAction(mo, (AbstractCreature)AbstractDungeon.player, new BurnPower(mo, potency), potency));
+         
+        
+        }
+      }
+    
+    @Override
+    public AbstractPotion makeCopy() {
+        return new FlameOrbPotion();
+    }
+
+    // This is your potency.
+    @Override
+    public int getPotency(final int potency) {
+        return 9;
+    }
+
+    public void upgradePotion()
+    {
+      potency += 9;
+      tips.clear();
+      tips.add(new PowerTip(name, description));
+    }
+}
