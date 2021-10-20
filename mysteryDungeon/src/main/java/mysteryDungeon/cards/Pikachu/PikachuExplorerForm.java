da4cb6117df5392232a3f4e7bf06741c16ed93c9
Ivel54 <abudjo55@gmail.com>
2021-10-20 17:37:46 -0400

    `pikachu Explorer form working

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Pikachu/PikachuExplorerForm.java
@@ -0,0 +1,71 @@
+package mysteryDungeon.cards.Pikachu;
+
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.defect.ChannelAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.orbs.Lightning;
+import com.megacrit.cardcrawl.powers.BiasPower;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonCard;
+import mysteryDungeon.characters.Pokemon;
+
+public class PikachuExplorerForm extends PokemonCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(PikachuExplorerForm.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath("PikachuPower.png");
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = Pokemon.Enums.PIKACHU_YELLOW;
+    
+
+    private static final int COST = 2;
+    private static final int BASE_MAGIC_NUMBER = 3;
+    private static final int UPGRADE_MAGIC_NUMBER = 2;
+
+
+    // /STAT DECLARATION/
+
+    public PikachuExplorerForm() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
+        isAdventurerOnly = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m){
+        for(int i=0; i<magicNumber;i++){
+        addToBot(new ChannelAction(new Lightning()));
+        }
+        addToBot(new ApplyPowerAction(p, p, new BiasPower(p, 1), 1));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
+            initializeDescription();
+        }
+    }
+}
