bfb830b1eddc9de6816efc392cc73d491a655321
Ivel54 <abudjo55@gmail.com>
2021-10-20 18:00:18 -0400

    charmander Explorer form is now online

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Charmander/CharmanderExplorerForm.java
@@ -0,0 +1,77 @@
+package mysteryDungeon.cards.Charmander;
+
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonCard;
+import mysteryDungeon.characters.Pokemon;
+import mysteryDungeon.powers.CharmanderExplorerPower;
+
+public class CharmanderExplorerForm extends PokemonCard {
+
+    /*
+     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
+     *
+     * Special Strike: Deal 7 (*) damage times the energy you currently have.
+     */
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(CharmanderExplorerForm.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath("CharmanderPower.png");
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = Pokemon.Enums.CHARMANDER_RED;
+
+    private static final int COST = 3;
+    private static final int BASE_MAGIC_NUMBER = 1;
+
+
+    // /STAT DECLARATION/
+
+    public CharmanderExplorerForm() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
+        isEthereal = true;
+        isAdventurerOnly = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        // Create an int which equals to your current energy.
+        addToBot(new ApplyPowerAction(p, p, new CharmanderExplorerPower(p, magicNumber), magicNumber));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            isEthereal = false;
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
