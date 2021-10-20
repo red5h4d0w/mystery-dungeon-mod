c9003e978167b35bec9a7a4d9699e35a0d75b9cb
Ivel54 <abudjo55@gmail.com>
2021-10-20 18:34:58 -0400

    smol detail

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Pikachu/PikachuExplorerForm.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Pikachu/PikachuExplorerForm.java
@@ -36,7 +36,7 @@ public class PikachuExplorerForm extends PokemonCard {
     public static final CardColor COLOR = Pokemon.Enums.PIKACHU_YELLOW;
     
 
-    private static final int COST = 2;
+    private static final int COST = 3;
     private static final int BASE_MAGIC_NUMBER = 3;
     private static final int UPGRADE_MAGIC_NUMBER = 2;
 
