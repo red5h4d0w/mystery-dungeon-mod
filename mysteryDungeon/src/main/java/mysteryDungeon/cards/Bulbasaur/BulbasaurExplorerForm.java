bfa70d077b78f51ec2e18f71d63662731fb048d2
Ivel54 <abudjo55@gmail.com>
2021-10-20 18:22:27 -0400

    Bulbasaur explorer form

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Bulbasaur/BulbasaurExplorerForm.java
@@ -0,0 +1,67 @@
+package mysteryDungeon.cards.Bulbasaur;
+
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.RegenPower;
+import com.megacrit.cardcrawl.powers.WraithFormPower;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonCard;
+import mysteryDungeon.characters.Pokemon;
+
+public class BulbasaurExplorerForm extends PokemonCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(BulbasaurExplorerForm.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath("BulbasaurPower.png");
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = Pokemon.Enums.BULBASAUR_GREEN;
+
+    private static final int COST = 3;
+    private static final int BASE_MAGIC_NUMBER = 9;
+    private static final int UPGRADE_MAGIC_NUMBER = 12;
+
+    // /STAT DECLARATION/
+
+    public BulbasaurExplorerForm() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        // Create an int which equals to your current energy.
+        addToBot(new ApplyPowerAction(p, p, new RegenPower(p, magicNumber), magicNumber));
+        addToBot(new ApplyPowerAction(p, p, new WraithFormPower(p, 1), 1));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
+            initializeDescription();
+            isEthereal = false;
+        }
+    }
+}
