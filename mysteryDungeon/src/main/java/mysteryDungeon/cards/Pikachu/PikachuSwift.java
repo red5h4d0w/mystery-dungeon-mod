af73d0863df4470004e1c917c530cc91d87905ac
Ivel54 <abudjo55@gmail.com>
2021-10-20 21:39:12 -0400

    other small change

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Pikachu/PikachuSwift.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Pikachu/PikachuSwift.java
@@ -31,7 +31,7 @@ public class PikachuSwift extends PokemonCard {
     // STAT DECLARATION
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = Pokemon.Enums.PIKACHU_YELLOW;
     
