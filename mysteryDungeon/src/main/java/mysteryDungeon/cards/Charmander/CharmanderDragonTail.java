8a3779639744419b280a97b1e63c0c809cc99db5
Ivel54 <abudjo55@gmail.com>
2021-11-17 13:07:01 -0500

    some changes for patch 6.5

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Charmander/CharmanderDragonTail.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Charmander/CharmanderDragonTail.java
@@ -8,7 +8,6 @@ import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
