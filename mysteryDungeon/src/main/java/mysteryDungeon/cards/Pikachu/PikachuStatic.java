1bbd45ccff180dc67a3b66e1b5197d0d1e86aa63
Ivel54 <abudjo55@gmail.com>
2021-12-13 20:06:20 -0500

    Pikachu Powers got art + Discharge

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Pikachu/PikachuStatic.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Pikachu/PikachuStatic.java
@@ -19,7 +19,7 @@ public class PikachuStatic extends PokemonCard {
 
     public static final String ID = MysteryDungeon.makeID(PikachuStatic.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("PikachuPower.png");
+    public static final String IMG = makeCardPath(PikachuStatic.class.getSimpleName()+".png");
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
