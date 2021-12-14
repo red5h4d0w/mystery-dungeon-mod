fc6da43db8017dc25168f8f973c5bdc74538c6a5
Ivel54 <abudjo55@gmail.com>
2021-12-13 22:03:25 -0500

    More PikachuAttacks

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Pikachu/PikachuZapCannon.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Pikachu/PikachuZapCannon.java
@@ -22,7 +22,7 @@ public class PikachuZapCannon extends PokemonCard {
 
     public static final String ID = MysteryDungeon.makeID(PikachuZapCannon.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("PikachuAttack.png");
+    public static final String IMG = makeCardPath(PikachuZapCannon.class.getSimpleName()+".png");
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
