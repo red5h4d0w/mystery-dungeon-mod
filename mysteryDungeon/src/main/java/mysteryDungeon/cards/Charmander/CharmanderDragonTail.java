e4cbe550d750cb3535d4f232aa0facf077f9f62e
Ivel54 <abudjo55@gmail.com>
2021-12-15 14:34:20 -0500

    All charmander Atk card art

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Charmander/CharmanderDragonTail.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Charmander/CharmanderDragonTail.java
@@ -25,7 +25,7 @@ public class CharmanderDragonTail extends PokemonCard {
 
     public static final String ID = MysteryDungeon.makeID(CharmanderDragonTail.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("CharmanderAttack.png");
+    public static final String IMG = makeCardPath(CharmanderDragonTail.class.getSimpleName()+".png");
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
