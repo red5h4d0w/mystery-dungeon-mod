b9ad31792c45b9d6a3ebe2c20f6de734aa9e1d96
Ivel54 <abudjo55@gmail.com>
2021-12-19 20:48:58 -0500

    All bulbasaur skill got art as well as some pikachu changes

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Bulbasaur/BulbasaurLeechSeed.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Bulbasaur/BulbasaurLeechSeed.java
@@ -19,7 +19,7 @@ public class BulbasaurLeechSeed extends PokemonCard {
 
     public static final String ID = MysteryDungeon.makeID(BulbasaurLeechSeed.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("BulbasaurSkill.png");
+    public static final String IMG = makeCardPath(BulbasaurLeechSeed.class.getSimpleName()+".png");
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 
