e9dd56d7245903f7d44ee6778e22d15fe2b2add4
Ivel54 <abudjo55@gmail.com>
2021-12-27 04:53:36 -0500

    Scald buff

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleScald.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Squirtle/SquirtleScald.java
@@ -36,8 +36,8 @@ public class SquirtleScald extends PokemonCard {
     private static final int COST = 1;
     private static final int DAMAGE = 4;
     private static final int UPGRADE_PLUS_DMG = 2;
-    private static final int BASE_MAGIC_NUMBER = 2;
-    private static final int UPGRADE_MAGIC_NUMBER = 2;
+    private static final int BASE_MAGIC_NUMBER = 4;
+    private static final int UPGRADE_MAGIC_NUMBER = 1;
     private static final int SECOND_MAGIC_NUMBER = 3;
     private static final int UPGRADE_SECONDMAGIC_NUMBER = 1;
 
