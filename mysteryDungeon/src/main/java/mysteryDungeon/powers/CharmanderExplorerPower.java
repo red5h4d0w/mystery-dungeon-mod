472782aa3beb7bed5e151b375b1690239637d933
Ivel54 <abudjo55@gmail.com>
2022-01-05 23:20:10 -0500

    attempt at art for powers

--- mysteryDungeon/src/main/java/mysteryDungeon/powers/CharmanderExplorerPower.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/powers/CharmanderExplorerPower.java
@@ -31,8 +31,8 @@ public class CharmanderExplorerPower extends MysteryDungeonPower implements Clon
 
     // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
     // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
-    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
-    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
+    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath(CharmanderExplorerPower.class.getSimpleName()+"84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath(CharmanderExplorerPower.class.getSimpleName()+"32.png"));
 
     public CharmanderExplorerPower(final AbstractCreature owner, final int amount) {
         name = NAME;
