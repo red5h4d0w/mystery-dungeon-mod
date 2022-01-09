8d82e6e720c886903a6ed8100fe4beca9297c9c1
red5h4d0w <bussiereismael@gmail.com>
2022-01-08 23:46:20 -0500

    evasiveness drop fixes

--- mysteryDungeon/src/main/java/mysteryDungeon/powers/EvasivenessDropPower.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/powers/EvasivenessDropPower.java
@@ -9,10 +9,9 @@ import static mysteryDungeon.MysteryDungeon.makePowerPath;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
+import mysteryDungeon.actions.EvasivenessDropAction;
+
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.PowerStrings;
@@ -31,6 +30,8 @@ public class EvasivenessDropPower extends MysteryDungeonPower implements Cloneab
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath(EvasivenessDropPower.class.getSimpleName()+"84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath(EvasivenessDropPower.class.getSimpleName()+"32.png"));
 
     public EvasivenessDropPower(final AbstractCreature owner, final int amount) {
         name = NAME;
@@ -42,29 +43,22 @@ public class EvasivenessDropPower extends MysteryDungeonPower implements Cloneab
         type = PowerType.DEBUFF;
         isTurnBased = true;
 
-       // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
-    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
-        private static final Texture tex84 = TextureLoader.getTexture(makePowerPath(EvasivenessDropPower.class.getSimpleName()+"84.png"));
-        private static final Texture tex32 = TextureLoader.getTexture(makePowerPath(EvasivenessDropPower.class.getSimpleName()+"32.png"));
+        // We load those txtures here.
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
     }
 
-    
     @Override
-    public void atEndOfRound() {
-        super.atStartOfTurn();
-        if (!owner.isDeadOrEscaped() && !owner.isDying) {
-            if(owner.currentBlock>0)
-            {
-                flash();
-                addToBot(new DamageAction(owner, new DamageInfo(owner, (int)Math.ceil(0.25f * owner.currentBlock), DamageType.THORNS)));
-            }
-            addToBot(new ReducePowerAction(owner, owner, this, 1));
-        }
+    public void atEndOfTurnPreEndTurnCards(boolean isPlayer) {
+        super.atEndOfTurnPreEndTurnCards(isPlayer);
+        flash();
+        addToBot(new EvasivenessDropAction(owner));
+        addToBot(new ReducePowerAction(owner, owner, this, 1));
     }
 
     @Override
     public AbstractPower makeCopy() {
-        return new LeechSeedPower(owner, source, amount);
+        return new EvasivenessDropPower(owner, amount);
     }
 
     @Override
