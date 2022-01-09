f5c8fb8eb320345e8f6d31956b84a6392e50fadd
Ivel54 <abudjo55@gmail.com>
2022-01-09 03:14:27 -0500

    modified stance effect only need to change particle effects

--- mysteryDungeon/src/main/java/mysteryDungeon/stances/NegativeStance.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/stances/NegativeStance.java
@@ -1,13 +1,20 @@
 package mysteryDungeon.stances;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.defect.EvokeOrbAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.AbstractCard.CardType;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.StanceStrings;
 import com.megacrit.cardcrawl.powers.FocusPower;
+import com.megacrit.cardcrawl.vfx.BorderFlashEffect;
+import com.megacrit.cardcrawl.vfx.stance.CalmParticleEffect;
+import com.megacrit.cardcrawl.vfx.stance.StanceAuraEffect;
 
 import mysteryDungeon.MysteryDungeon;
 import mysteryDungeon.abstracts.PokemonCard;
@@ -21,13 +28,35 @@ public class NegativeStance extends PokemonStance {
     
     private static final StanceStrings stanceString = CardCrawlGame.languagePack.getStanceString(STANCE_ID);
     private int timesActivatedThisTurn = 0;
-  
+    private static long sfxId = -1L;
     public NegativeStance() {
         ID = STANCE_ID;
         name = stanceString.NAME;
         updateDescription();
     }
-  
+
+    public void updateAnimation() {
+        if (!Settings.DISABLE_EFFECTS) {
+          this.particleTimer -= Gdx.graphics.getDeltaTime();
+          if (this.particleTimer < 0.0F) {
+            this.particleTimer = 0.04F;
+            AbstractDungeon.effectsQueue.add(new CalmParticleEffect());
+          } 
+        } 
+        this.particleTimer2 -= Gdx.graphics.getDeltaTime();
+        if (this.particleTimer2 < 0.0F) {
+          this.particleTimer2 = MathUtils.random(0.45F, 0.55F);
+          AbstractDungeon.effectsQueue.add(new StanceAuraEffect("Calm"));
+        } 
+      }
+      
+      public void onEnterStance() {
+        if (sfxId != -1L)
+          stopIdleSfx(); 
+        CardCrawlGame.sound.play("POWER_FOCUS", 0.05F);
+        AbstractDungeon.effectsQueue.add(new BorderFlashEffect(Color.YELLOW, true));
+      }
+
     @Override
     public void onPlayCard(AbstractCard card) {
         if(card instanceof PokemonCard) {
@@ -61,6 +90,12 @@ public class NegativeStance extends PokemonStance {
     @Override
     public void onExitStance() {
         addToBot(new EvokeOrbAction(1));
+        stopIdleSfx();
     }
-
+    public void stopIdleSfx() {
+        if (sfxId != -1L) {
+          CardCrawlGame.sound.stop("STANCE_LOOP_CALM", sfxId);
+          sfxId = -1L;
+        } 
+}
 }
--- mysteryDungeon/src/main/java/mysteryDungeon/stances/PositiveStance.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/stances/PositiveStance.java
@@ -1,12 +1,20 @@
 package mysteryDungeon.stances;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.AbstractCard.CardType;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.StanceStrings;
 import com.megacrit.cardcrawl.orbs.Lightning;
+import com.megacrit.cardcrawl.vfx.BorderFlashEffect;
+import com.megacrit.cardcrawl.vfx.stance.StanceAuraEffect;
+import com.megacrit.cardcrawl.vfx.stance.StanceChangeParticleGenerator;
+import com.megacrit.cardcrawl.vfx.stance.WrathParticleEffect;
 
 import mysteryDungeon.MysteryDungeon;
 import mysteryDungeon.abstracts.PokemonCard;
@@ -20,13 +28,35 @@ public class PositiveStance extends PokemonStance {
     
     private static final StanceStrings stanceString = CardCrawlGame.languagePack.getStanceString(STANCE_ID);
   
-  
+    private static long sfxId = -1L;
+
     public PositiveStance() {
         ID = STANCE_ID;
         name = stanceString.NAME;
         updateDescription();
     }
-  
+    public void updateAnimation() {
+        if (!Settings.DISABLE_EFFECTS) {
+          this.particleTimer -= Gdx.graphics.getDeltaTime();
+          if (this.particleTimer < 0.0F) {
+            this.particleTimer = 0.05F;
+            AbstractDungeon.effectsQueue.add(new WrathParticleEffect());
+          } 
+        } 
+        this.particleTimer2 -= Gdx.graphics.getDeltaTime();
+        if (this.particleTimer2 < 0.0F) {
+          this.particleTimer2 = MathUtils.random(0.3F, 0.4F);
+          AbstractDungeon.effectsQueue.add(new StanceAuraEffect("Wrath"));
+        } 
+      }
+    public void onEnterStance() {
+        if (sfxId != -1L)
+          stopIdleSfx(); 
+        CardCrawlGame.sound.play("ORB_LIGHTNING_EVOKE");
+        AbstractDungeon.effectsQueue.add(new BorderFlashEffect(Color.YELLOW, true));
+        AbstractDungeon.effectsQueue.add(new StanceChangeParticleGenerator(AbstractDungeon.player.hb.cX, AbstractDungeon.player.hb.cY, "Wrath"));
+  }
+
     @Override
     public void onPlayCard(AbstractCard card) {
         if(card instanceof PokemonCard) {
@@ -54,6 +84,12 @@ public class PositiveStance extends PokemonStance {
     @Override
     public void onExitStance() {
         addToBot(new GainBlockAction(AbstractDungeon.player, AbstractDungeon.player, 4));
+        stopIdleSfx();
     }
-
+    public void stopIdleSfx() {
+        if (sfxId != -1L) {
+          CardCrawlGame.sound.stop("STANCE_LOOP_WRATH", sfxId);
+          sfxId = -1L;
+        } 
+}
 }
