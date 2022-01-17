2d52de0ebf592fe304360719a1c285c38226da80
Ivel54 <abudjo55@gmail.com>
2022-01-17 13:05:47 -0500

    leech seed almost works

--- mysteryDungeon/src/main/java/mysteryDungeon/powers/LeechSeedPower.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/powers/LeechSeedPower.java
@@ -10,10 +10,11 @@ import static mysteryDungeon.MysteryDungeon.makePowerPath;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-
+import com.badlogic.gdx.math.MathUtils;
 import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.HealthBarRenderPower;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
 import com.megacrit.cardcrawl.core.AbstractCreature;
@@ -31,6 +32,7 @@ public class LeechSeedPower extends MysteryDungeonPower implements CloneablePowe
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    public int wasAttacked = 0;
 
     // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
     // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
@@ -73,6 +75,21 @@ public class LeechSeedPower extends MysteryDungeonPower implements CloneablePowe
     public Color getColor()
     {
         return Color.OLIVE;
+    }
+    public int onAttacked(DamageInfo info, int damageAmount) {
+        wasAttacked++;
+        return damageAmount;
+    }
+
+    public void atEndOfRound(){
+    if(wasAttacked<2){
+        if(amount<=1){
+            addToBot(new RemoveSpecificPowerAction(owner, owner, this));
+        }
+        else
+            amount-= MathUtils.ceil(amount/2.0f);
+        updateDescription();
+        }
     }
 
     @Override
