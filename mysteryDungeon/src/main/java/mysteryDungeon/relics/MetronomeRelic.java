016bfb6a8dc0b1672b08c32e065c1e09b0f89c90
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2022-01-17 10:52:29 -0500

    Revert "deleted metronome for now"
    This reverts commit 04b40505962c68df0e131282ec2f06a3ef834e71.

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/relics/MetronomeRelic.java
@@ -0,0 +1,52 @@
+package mysteryDungeon.relics;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonRelic;
+import mysteryDungeon.util.TextureLoader;
+
+import static mysteryDungeon.MysteryDungeon.makeRelicOutlinePath;
+import static mysteryDungeon.MysteryDungeon.makeRelicPath;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.localization.RelicStrings;
+
+public class MetronomeRelic extends PokemonRelic { 
+
+    // ID, images, text.
+    public static final String ID = MysteryDungeon.makeID(MetronomeRelic.class.getSimpleName());
+
+    private static final RelicStrings relicStrings = CardCrawlGame.languagePack.getRelicStrings(ID);
+    public static final String NAME = relicStrings.NAME;
+    public static final String[] DESCRIPTIONS = relicStrings.DESCRIPTIONS;
+    private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("metronome.png"));
+    private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("metronome.png"));
+
+    public MetronomeRelic() {
+        super(ID, IMG, OUTLINE, RelicTier.SHOP, LandingSound.CLINK);
+
+        tips.clear();
+        tips.add(new PowerTip(name, description));
+    }
+
+    @Override
+    public void atTurnStart() {
+      if (!AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
+        flash();
+          AbstractCard c = AbstractDungeon.returnTrulyRandomCard();
+          c.setCostForTurn(0);
+          addToBot((AbstractGameAction)new MakeTempCardInHandAction(c, true));
+          
+      } 
+    }
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+
+}
