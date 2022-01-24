b13ef252d5520885a378fa77189cdfb6e1e6157c
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2022-01-24 13:45:14 -0500

    is this good :pensive:

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Chikorita/ChikoritaRecover.java
@@ -0,0 +1,62 @@
+package mysteryDungeon.cards.Chikorita;
+
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonCard;
+import mysteryDungeon.characters.Pokemon;
+import mysteryDungeon.powers.RecoverPower;
+
+public class ChikoritaRecover extends PokemonCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(ChikoritaRecover.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath("Skill.png");
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.BASIC;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Pokemon.Enums.CHIKORITA_GREEN;
+
+    private static final int COST = 0;
+    private static final int BASE_MAGIC_NUMBER = 0;
+
+
+    // /STAT DECLARATION/
+
+    public ChikoritaRecover() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new RecoverPower(p, 0), 0));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            initializeDescription();
+        }
+    }
+}
---
+++ mysteryDungeon/src/main/java/mysteryDungeon/powers/RecoverPower.java
@@ -0,0 +1,91 @@
+package mysteryDungeon.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.util.TextureLoader;
+
+import static mysteryDungeon.MysteryDungeon.makePowerPath;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.MathUtils;
+import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.HealthBarRenderPower;
+
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+
+//Gain 1 dex for the turn for each card played.
+
+public class RecoverPower extends MysteryDungeonPower implements CloneablePowerInterface, HealthBarRenderPower {
+    public AbstractCreature source;
+
+    public static final String POWER_ID = MysteryDungeon.makeID("DelayedHealPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath(RecoverPower.class.getSimpleName()+"84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath(RecoverPower.class.getSimpleName()+"32.png"));
+
+    public RecoverPower(final AbstractCreature owner, final int amount) {
+        name = NAME;
+        ID = POWER_ID;
+
+        this.owner = owner;
+        this.amount = amount;
+
+        type = PowerType.DEBUFF;
+        isTurnBased = false;
+
+        // We load those txtures here.
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    public int getHealthBarAmount() {
+        return -Math.min(owner.maxHealth - owner.currentHealth, amount);
+    }
+
+    @Override
+    public void onAttack(DamageInfo info, int damageAmount, AbstractCreature target) {
+        addToBot(new HealAction(owner, owner, Math.min(MathUtils.floor(0.2f*damageAmount),amount)));
+        super.onAttack(info, damageAmount, target);
+    }
+
+    @Override
+    public int onAttacked(DamageInfo info, int damageAmount) {
+        amount += MathUtils.floor(damageAmount);
+        return super.onAttacked(info, damageAmount);
+    }
+
+    @Override
+    public int onHeal(int healAmount) {
+        amount -= healAmount;
+        return super.onHeal(healAmount);
+    }
+
+    public Color getColor()
+    {
+        return Color.GREEN.cpy();
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new RecoverPower(owner, amount);
+    }
+
+    @Override
+    public void updateDescription() {
+        description = String.format(DESCRIPTIONS[0], amount);
+    }
+}
