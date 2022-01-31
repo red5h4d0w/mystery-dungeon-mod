02c9a0a6538d835bbe69afae80bb46448d03ee2c
Ivel54 <abudjo55@gmail.com>
2022-01-31 12:45:37 -0500

    Grass whistle added

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Chikorita/ChikoritaGrassWhistle.java
@@ -0,0 +1,74 @@
+package mysteryDungeon.cards.Chikorita;
+
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonCard;
+import mysteryDungeon.characters.Pokemon;
+import mysteryDungeon.powers.AsleepPower;
+
+public class ChikoritaGrassWhistle extends PokemonCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(ChikoritaGrassWhistle.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath("Skill.png");
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Pokemon.Enums.CHIKORITA_GREEN;
+
+    private static final int COST = 1;
+    private static final int BASE_MAGIC_NUMBER = 1;
+
+
+    // /STAT DECLARATION/
+
+    public ChikoritaGrassWhistle() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
+        exhaust = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (!AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
+            for (AbstractMonster monster : (AbstractDungeon.getCurrRoom()).monsters.monsters) {
+                if (!monster.isDead && !monster.isDying) {
+                    addToBot(new ApplyPowerAction(monster, p, new AsleepPower(monster, magicNumber), this.magicNumber));
+                } 
+            } 
+        }
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            selfRetain = true;
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
