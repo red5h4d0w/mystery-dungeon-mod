413bf97acc68aadca4f87c0c26161f8a99bf7174
Ivel54 <abudjo55@gmail.com>
2022-03-07 23:43:21 -0500

    pledge's start    it's hard

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Chikorita/ChikoritaGrassPledge.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Chikorita/ChikoritaGrassPledge.java
@@ -2,7 +2,7 @@ package mysteryDungeon.cards.Chikorita;
 
 import static mysteryDungeon.MysteryDungeon.makeCardPath;
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
@@ -11,45 +11,43 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import mysteryDungeon.MysteryDungeon;
 import mysteryDungeon.abstracts.PokemonCard;
 import mysteryDungeon.characters.Pokemon;
-import mysteryDungeon.powers.RecoverPower;
 
-public class ChikoritaRecover extends PokemonCard {
+public class ChikoritaGrassPledge extends PokemonCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = MysteryDungeon.makeID(ChikoritaRecover.class.getSimpleName());
+    public static final String ID = MysteryDungeon.makeID(ChikoritaGrassPledge.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String IMG = makeCardPath("Skill.png");
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
 
     // /TEXT DECLARATION/
 
-
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.BASIC;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = Pokemon.Enums.CHIKORITA_GREEN;
 
-    private static final int COST = 0;
-    private static final int BASE_MAGIC_NUMBER = 0;
-
+    private static final int COST = 1;
+    private static final int BASE_MAGIC_NUMBER = 6;
+    private static final int UPGRADE_MAGIC_NUMBER = 3;
 
     // /STAT DECLARATION/
 
-    public ChikoritaRecover() {
+    public ChikoritaGrassPledge() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
         baseMagicNumber = BASE_MAGIC_NUMBER;
         magicNumber = baseMagicNumber;
-        exhaust = true;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new ApplyPowerAction(p, p, new RecoverPower(p, 0), 0));
+        addToBot(new AddTemporaryHPAction(p, p, magicNumber));
     }
 
     // Upgraded stats.
@@ -57,6 +55,8 @@ public class ChikoritaRecover extends PokemonCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
+            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
+            rawDescription = UPGRADE_DESCRIPTION;
             initializeDescription();
         }
     }
---
+++ mysteryDungeon/src/main/java/mysteryDungeon/powers/GrassPledgePower.java
@@ -0,0 +1,97 @@
+package mysteryDungeon.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonPower;
+import mysteryDungeon.interfaces.onDyingInterface;
+import mysteryDungeon.util.TextureLoader;
+
+import static mysteryDungeon.MysteryDungeon.makePowerPath;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardQueueItem;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+
+//Gain 1 dex for the turn for each card played.
+
+public class GrassPledgePower extends PokemonPower implements CloneablePowerInterface, onDyingInterface {
+    public AbstractCreature source;
+
+    public static final String POWER_ID = MysteryDungeon.makeID("GrassPledgePower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath(GrassPledgePower.class.getSimpleName()+"84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath(GrassPledgePower.class.getSimpleName()+"32.png"));
+
+    public GrassPledgePower(final AbstractCreature owner, final int amount) {
+        name = NAME;
+        ID = POWER_ID;
+
+        this.owner = owner;
+        this.amount = amount;
+
+        type = PowerType.BUFF;
+
+        // We load those txtures here.
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+    
+        updateDescription();
+    }
+
+    
+    public void onUseCard(com.megacrit.cardcrawl.powers.AbstractPower power, com.megacrit.cardcrawl.core.AbstractCreature target, com.megacrit.cardcrawl.core.AbstractCreature source) {
+        if(AbstractPower.power == new(WaterPledgePower(AbstractPlayer.player, AbstractPlayer.player))){
+            if (!AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
+                for (AbstractMonster monster : (AbstractDungeon.getMonsters()).monsters) {
+                    if (!monster.isDead && !monster.isDying) {
+                        addToBot(new ApplyPowerAction(AbstractMonster.monster, AbstractPlayer.p, new WeakPower(AbstractMonster.monster, AbstractPlayer.p, 2), 2));
+                    }
+                } 
+            } 
+        }
+        if(AbstractPower.power == new(FirePledgePower(AbstractPlayer.player, AbstractPlayer.player))){
+            if (!AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
+                for (AbstractMonster monster : (AbstractDungeon.getMonsters()).monsters) {
+                    if (!monster.isDead && !monster.isDying) {
+                        addToBot(new ApplyPowerAction(AbstractMonster.monster, AbstractPlayer.p, new PoisonPower(AbstractMonster.monster, AbstractPlayer.p, 9), 9));
+                    }
+                } 
+            } 
+        }
+    }
+
+    public void atEndOfTurn(boolean isPlayer) {
+        if (isPlayer)
+          addToBot(new RemoveSpecificPowerAction(owner, owner, this));
+}
+    @Override
+    public AbstractPower makeCopy() {
+        return new GrassPledgePower(owner, amount);
+    }
+
+    @Override
+    public void updateDescription() {
+        description = String.format(DESCRIPTIONS[0], amount);
+    }
+}
+    
+        
+    
