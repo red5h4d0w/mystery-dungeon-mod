8152c31107aaa0a566495a35eb4d58ac95cd11b1
Ivel54 <abudjo55@gmail.com>
2022-05-03 00:48:09 -0400

    magical leaf small fix

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Chikorita/ChikoritaMagicalLeaf.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Chikorita/ChikoritaMagicalLeaf.java
@@ -28,7 +28,7 @@ public class ChikoritaMagicalLeaf extends PokemonCard {
     // STAT DECLARATION
 
     private static final CardRarity RARITY = CardRarity.COMMON;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = Pokemon.Enums.CHIKORITA_GREEN;
 
