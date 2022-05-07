0f70a77513e8a719aa0c77c9ef254d07b130447e
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2022-05-07 11:29:51 -0400

    overgrow power

--- mysteryDungeon/src/main/java/mysteryDungeon/powers/OvergrowPower.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/powers/OvergrowPower.java
@@ -7,16 +7,10 @@ import mysteryDungeon.util.TextureLoader;
 
 import static mysteryDungeon.MysteryDungeon.makePowerPath;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-// import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.HealthBarRenderPower;
-import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.PowerStrings;
@@ -25,10 +19,10 @@ import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 
 //Gain 1 dex for the turn for each card played.
 
-public class GrassyTerrainPower extends PokemonTwoAmountPower implements  CloneablePowerInterface {
+public class OvergrowPower extends PokemonTwoAmountPower implements CloneablePowerInterface {
     public AbstractCreature source;
 
-    public static final String POWER_ID = MysteryDungeon.makeID("GrassyTerrainPower");
+    public static final String POWER_ID = MysteryDungeon.makeID("OvergrowPower");
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
@@ -38,11 +32,11 @@ public class GrassyTerrainPower extends PokemonTwoAmountPower implements  Clonea
     // There's a fallback "missing texture" image, so the game shouldn't crash if
     // you accidentally put a non-existent file.
     private static final Texture tex84 = TextureLoader
-            .getTexture(makePowerPath(GrassyTerrainPower.class.getSimpleName() + "84.png"));
+            .getTexture(makePowerPath(OvergrowPower.class.getSimpleName() + "84.png"));
     private static final Texture tex32 = TextureLoader
-            .getTexture(makePowerPath(GrassyTerrainPower.class.getSimpleName() + "32.png"));
+            .getTexture(makePowerPath(OvergrowPower.class.getSimpleName() + "32.png"));
 
-    public GrassyTerrainPower(final AbstractCreature owner, final int amount) {
+    public OvergrowPower(final AbstractCreature owner, final int amount) {
         name = NAME;
         ID = POWER_ID;
 
@@ -60,17 +54,15 @@ public class GrassyTerrainPower extends PokemonTwoAmountPower implements  Clonea
         updateDescription();
     }
 
-
     @Override
     public void wasHPLost(DamageInfo info, int damageAmount) {
-        if(AbstractCreature(AbstractPlayer.player.currentHealth < AbstractPlayer.p.maxHealth * 0.5f)
-        addToBot(new ApplyPowerAction(AbstractCreature(AbstractPlayer.player)), AbstractCreature(AbstractPlayer.player), new VigorPower(AbstractCreature(AbstractPlayer.player), amount));
-        return;
+        if(owner.currentHealth < owner.maxHealth * 0.5f && owner.currentHealth + damageAmount > 0.5f * owner.maxHealth)
+            addToBot(new ApplyPowerAction(owner, owner, new VigorPower(owner, amount), amount));
     }
 
     @Override
     public AbstractPower makeCopy() {
-        return new GrassyTerrainPower(owner, amount);
+        return new OvergrowPower(owner, amount);
     }
 
     @Override
