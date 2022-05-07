d08fb76e6b94173cd854d67393e1a56450b26b3e
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2022-05-07 11:36:54 -0400

    Update ChikoritaWorrySeedPower.java

--- mysteryDungeon/src/main/java/mysteryDungeon/powers/ChikoritaWorrySeedPower.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/powers/ChikoritaWorrySeedPower.java
@@ -9,7 +9,6 @@ import static mysteryDungeon.MysteryDungeon.makePowerPath;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.actions.common.LoseHPAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
@@ -21,19 +20,22 @@ import com.megacrit.cardcrawl.powers.AbstractPower;
 
 //Gain 1 dex for the turn for each card played.
 
-public class ChikoritaWorrySeedPower extends PokemonPower implements CloneablePowerInterface{
+public class ChikoritaWorrySeedPower extends PokemonPower implements CloneablePowerInterface {
     public AbstractCreature source;
 
     public static final String POWER_ID = MysteryDungeon.makeID("ChikoritaWorrySeedPower");
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
-    
 
-    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
-    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
-    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath(ChikoritaWorrySeedPower.class.getSimpleName()+"84.png"));
-    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath(ChikoritaWorrySeedPower.class.getSimpleName()+"32.png"));
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84
+    // image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if
+    // you accidentally put a non-existent file.
+    private static final Texture tex84 = TextureLoader
+            .getTexture(makePowerPath(ChikoritaWorrySeedPower.class.getSimpleName() + "84.png"));
+    private static final Texture tex32 = TextureLoader
+            .getTexture(makePowerPath(ChikoritaWorrySeedPower.class.getSimpleName() + "32.png"));
 
     public ChikoritaWorrySeedPower(final AbstractCreature owner, final int amount) {
         name = NAME;
@@ -51,15 +53,19 @@ public class ChikoritaWorrySeedPower extends PokemonPower implements CloneablePo
         updateDescription();
     }
 
-    public void triggerOnOtherCardPlayed(AbstractCard c) {
-        addToTop((AbstractGameAction)new LoseHPAction((AbstractCreature)AbstractDungeon.player, (AbstractCreature)AbstractDungeon.player, amount));
-      }
+    @Override
+    public void onAfterCardPlayed(AbstractCard usedCard) {
+        super.onAfterCardPlayed(usedCard);
+        addToBot(new LoseHPAction(owner, owner, amount));
+    }
 
     public void atEndOfTurn(boolean isPlayer) {
         if (isPlayer)
-        addToBot(new HealAction((AbstractCreature)AbstractDungeon.player, (AbstractCreature)AbstractDungeon.player, 2 * amount));
-          
-}
+            addToBot(new HealAction((AbstractCreature) AbstractDungeon.player,
+                    (AbstractCreature) AbstractDungeon.player, 2 * amount));
+
+    }
+
     @Override
     public AbstractPower makeCopy() {
         return new ChikoritaWorrySeedPower(owner, amount);
@@ -67,12 +73,9 @@ public class ChikoritaWorrySeedPower extends PokemonPower implements CloneablePo
 
     @Override
     public void updateDescription() {
-        if(amount == 1)
-        {
+        if (amount == 1) {
             description = DESCRIPTIONS[0];
-        } 
-        else
-        {
+        } else {
             description = String.format(DESCRIPTIONS[1], amount);
         }
     }
