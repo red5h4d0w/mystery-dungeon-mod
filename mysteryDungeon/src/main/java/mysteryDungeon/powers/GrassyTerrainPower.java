aa13f7810618258025e919877351a5e73294bfa3
Ivel54 <abudjo55@gmail.com>
2022-05-07 00:27:58 -0400

    same

--- mysteryDungeon/src/main/java/mysteryDungeon/powers/GrassyTerrainPower.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/powers/GrassyTerrainPower.java
@@ -11,21 +11,24 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.MathUtils;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 // import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.HealthBarRenderPower;
 import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 
 //Gain 1 dex for the turn for each card played.
 
-public class RecoverPower extends PokemonTwoAmountPower implements  CloneablePowerInterface {
+public class GrassyTerrainPower extends PokemonTwoAmountPower implements  CloneablePowerInterface {
     public AbstractCreature source;
 
-    public static final String POWER_ID = MysteryDungeon.makeID("RecoverPower");
+    public static final String POWER_ID = MysteryDungeon.makeID("GrassyTerrainPower");
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
@@ -35,11 +38,11 @@ public class RecoverPower extends PokemonTwoAmountPower implements  CloneablePow
     // There's a fallback "missing texture" image, so the game shouldn't crash if
     // you accidentally put a non-existent file.
     private static final Texture tex84 = TextureLoader
-            .getTexture(makePowerPath(RecoverPower.class.getSimpleName() + "84.png"));
+            .getTexture(makePowerPath(GrassyTerrainPower.class.getSimpleName() + "84.png"));
     private static final Texture tex32 = TextureLoader
-            .getTexture(makePowerPath(RecoverPower.class.getSimpleName() + "32.png"));
+            .getTexture(makePowerPath(GrassyTerrainPower.class.getSimpleName() + "32.png"));
 
-    public RecoverPower(final AbstractCreature owner, final int amount) {
+    public GrassyTerrainPower(final AbstractCreature owner, final int amount) {
         name = NAME;
         ID = POWER_ID;
 
@@ -57,42 +60,17 @@ public class RecoverPower extends PokemonTwoAmountPower implements  CloneablePow
         updateDescription();
     }
 
-    // public int getHealthBarAmount() {
-    //     return -Math.min(owner.maxHealth - owner.currentHealth, amount);
-    // }
-
-    @Override
-    public void onAttack(DamageInfo info, int damageAmount, AbstractCreature target) {
-        if(info.type!=DamageType.NORMAL) {
-            super.onAttack(info, damageAmount, target);
-            return;
-        }
-        addToBot(new HealAction(owner, owner, Math.min(MathUtils.floor(0.3f * damageAmount), amount)));
-        amount -= Math.min(MathUtils.floor(0.25f * damageAmount), amount);
-        super.onAttack(info, damageAmount, target);
-    }
 
     @Override
     public void wasHPLost(DamageInfo info, int damageAmount) {
-        amount2 += MathUtils.floor(damageAmount);
+        if(AbstractCreature(AbstractPlayer.player.currentHealth < AbstractPlayer.p.maxHealth * 0.5f)
+        addToBot(new ApplyPowerAction(AbstractCreature(AbstractPlayer.player)), AbstractCreature(AbstractPlayer.player), new VigorPower(AbstractCreature(AbstractPlayer.player), amount));
         return;
     }
 
     @Override
-    public void atEndOfRound() {
-        super.atEndOfRound();
-        amount = amount2;
-        amount2 = 0;
-        updateDescription();
-    }
-
-    public Color getColor() {
-        return Color.GREEN.cpy();
-    }
-
-    @Override
     public AbstractPower makeCopy() {
-        return new RecoverPower(owner, amount);
+        return new GrassyTerrainPower(owner, amount);
     }
 
     @Override
