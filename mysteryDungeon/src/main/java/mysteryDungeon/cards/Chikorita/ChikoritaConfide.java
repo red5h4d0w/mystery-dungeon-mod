369a57cc4ce62c44e48bc04ac23d3ab3bec2381e
Ivel54 <abudjo55@gmail.com>
2022-05-08 16:31:02 -0400

    confide buff

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Chikorita/ChikoritaConfide.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Chikorita/ChikoritaConfide.java
@@ -37,7 +37,7 @@ public class ChikoritaConfide extends PokemonCard {
 
     private static final int COST = 2;
     private static final int BASE_MAGIC_NUMBER = 3;
-    private static final int UPGRADE_MAGIC_NUMBER = 1;
+    private static final int UPGRADE_MAGIC_NUMBER = 2;
 
 
     // /STAT DECLARATION/
