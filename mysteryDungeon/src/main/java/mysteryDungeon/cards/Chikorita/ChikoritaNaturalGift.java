aa5595686bd2057d2f470f616cac559c3b5237d1
Ivel54 <abudjo55@gmail.com>
2022-05-08 13:58:52 -0400

    New version of Sunny Day

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Chikorita/ChikoritaNaturalGift.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Chikorita/ChikoritaNaturalGift.java
@@ -35,7 +35,7 @@ public class ChikoritaNaturalGift extends PokemonCard {
     public static final CardColor COLOR = Pokemon.Enums.CHIKORITA_GREEN;
 
     private static final int COST = 1;
-    private static final int BASE_MAGIC_NUMBER = 2;
+    private static final int BASE_MAGIC_NUMBER = 1;
     private static final int UPGRADE_MAGIC_NUMBER = 1;
     private static final int BASE_SECOND_MAGIC_NUMBER = 1;
     private static final int UPGRADE_SECOND_MAGIC_NUMBER = 1;
--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Chikorita/ChikoritaSunnyDay.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Chikorita/ChikoritaSunnyDay.java
@@ -7,13 +7,12 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.DrawPower;
-
+import com.megacrit.cardcrawl.powers.watcher.VigorPower;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 
 import mysteryDungeon.MysteryDungeon;
 import mysteryDungeon.abstracts.PokemonCard;
 import mysteryDungeon.characters.Pokemon;
-import mysteryDungeon.powers.ReduceHighestCostAtStartOfTurnPower;
 
 public class ChikoritaSunnyDay extends PokemonCard {
 
@@ -21,10 +20,9 @@ public class ChikoritaSunnyDay extends PokemonCard {
 
     public static final String ID = MysteryDungeon.makeID(ChikoritaSunnyDay.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("Power.png");
+    public static final String IMG = makeCardPath("Skill.png");
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
 
     // /TEXT DECLARATION/
 
@@ -33,26 +31,40 @@ public class ChikoritaSunnyDay extends PokemonCard {
 
     private static final CardRarity RARITY = CardRarity.RARE;
     private static final CardTarget TARGET = CardTarget.SELF;
-    private static final CardType TYPE = CardType.POWER;
+    private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = Pokemon.Enums.CHIKORITA_GREEN;
 
-    private static final int COST = 2;
+    private static final int COST = -1;
+    private static final int UPGRADE_MAGIC_NUMBER = 3;
+
 
     // /STAT DECLARATION/
 
     public ChikoritaSunnyDay() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-        baseMagicNumber = 1;
+        baseMagicNumber = 7;
         magicNumber = baseMagicNumber;
-        isEthereal = true;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         // Create an int which equals to your current energy.
-        addToBot(new ApplyPowerAction(p, p, new DrawPower(p, magicNumber), magicNumber));
-        addToBot(new ApplyPowerAction(p, p, new ReduceHighestCostAtStartOfTurnPower(p, 1), 1));
+        if (energyOnUse < EnergyPanel.totalCount)
+        {
+            energyOnUse = EnergyPanel.totalCount; 
+        }
+        if (p.hasRelic("Chemical X")) {
+            energyOnUse += 2;
+            p.getRelic("Chemical X").flash();
+        }
+        for (int i = 0; i < this.energyOnUse; i++){
+            if(energyOnUse>0)
+                addToBot(new ApplyPowerAction(p, p, new VigorPower(p, magicNumber), magicNumber));
+            if (!freeToPlayOnce)
+                p.energy.use(EnergyPanel.totalCount);
+        }
+    
     }
 
     // Upgraded stats.
@@ -60,9 +72,8 @@ public class ChikoritaSunnyDay extends PokemonCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            rawDescription = UPGRADE_DESCRIPTION;
+            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
             initializeDescription();
-            isEthereal = false;
         }
     }
 }
