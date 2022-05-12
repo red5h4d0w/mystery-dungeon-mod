7a0dd2c1d4bae92d3d5cac5216fca1c08af9a000
Ivel54 <abudjo55@gmail.com>
2022-05-12 02:40:39 -0400

    potion and relicsto be tested and some  dont workto be finished on sunday

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/potions/GrassGemPotion.java
@@ -0,0 +1,84 @@
+package mysteryDungeon.potions;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.localization.PotionStrings;
+import com.megacrit.cardcrawl.potions.AbstractPotion;
+import com.megacrit.cardcrawl.powers.watcher.VigorPower;
+
+import mysteryDungeon.abstracts.PokemonPotion;
+import mysteryDungeon.pokemons.Squirtle;
+
+
+public class GrassGemPotion extends PokemonPotion {
+
+    public static final String POTION_ID = mysteryDungeon.MysteryDungeon.makeID("GrassGemPotion");
+    private static final PotionStrings potionStrings = CardCrawlGame.languagePack.getPotionString(POTION_ID);
+    
+    public static final String NAME = potionStrings.NAME;
+    public static final String[] DESCRIPTIONS = potionStrings.DESCRIPTIONS;
+
+    public static final String IMG_STRING = "x-atk.png";
+
+    public GrassGemPotion() {
+        // The bottle shape and inside is determined by potion size and color. The actual colors are the main MysteryDungeon.java
+        super(NAME, POTION_ID, PotionRarity.UNCOMMON, IMG_STRING);
+        
+        // Potency is the damage/magic number equivalent of potions.
+        potency = getPotency();
+        
+       // Do you throw this potion at an enemy or do you just consume it.
+        isThrown = false;
+        cardColor = Squirtle.CARD_COLOR;
+
+
+        updateDescription();
+        
+        // Initialize the on-hover name + description
+        tips.add(new PowerTip(name, description));
+        
+    }
+    // See that description? It has DESCRIPTIONS[1] instead of just hard-coding the "text " + potency + " more text" inside.
+    // DO NOT HARDCODE YOUR STRINGS ANYWHERE, it's really bad practice to have "Strings" in your code:
+
+    /*
+     * 1. It's bad for if somebody likes your mod enough (or if you decide) to translate it.
+     * Having only the JSON files for translation rather than 15 different instances of "Dexterity" in some random cards is A LOT easier.
+     *
+     * 2. You don't have a centralised file for all strings for easy proof-reading. If you ever want to change a string
+     * you don't have to go through all your files individually/pray that a mass-replace doesn't screw something up.
+     *
+     * 3. Without hardcoded strings, editing a string doesn't require a compile, saving you time (unless you clean+package).
+     *
+     */
+
+    @Override
+    public void use(AbstractCreature target) {
+        addToBot(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new VigorPower(AbstractDungeon.player, potency), potency));
+      }
+    
+    @Override
+    public AbstractPotion makeCopy() {
+        return new GrassGemPotion();
+    }
+
+    // This is your potency.
+    @Override
+    public int getPotency(final int potency) {
+        return 10;
+    }
+
+    public void upgradePotion()
+    {
+        potency += 10;
+        tips.clear();
+        tips.add(new PowerTip(name, description));
+    }
+
+    public void updateDescription() {
+        description = String.format(DESCRIPTIONS[0], potency);
+    }
+}
---
+++ mysteryDungeon/src/main/java/mysteryDungeon/relics/GrassySeedRelic.java
@@ -0,0 +1,70 @@
+package mysteryDungeon.relics;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonCard;
+import mysteryDungeon.abstracts.PokemonRelic;
+import mysteryDungeon.pokemons.Charmander;
+import mysteryDungeon.util.TextureLoader;
+
+import static mysteryDungeon.MysteryDungeon.makeRelicOutlinePath;
+import static mysteryDungeon.MysteryDungeon.makeRelicPath;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.actions.common.LoseHPAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.AbstractCard.CardColor;
+import com.megacrit.cardcrawl.cards.AbstractCard.CardTags;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
+import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.localization.RelicStrings;
+
+public class GrassySeedRelic extends PokemonRelic { 
+
+    // ID, images, text.
+    public static final String ID = MysteryDungeon.makeID(GrassySeedRelic.class.getSimpleName());
+
+    private static final RelicStrings relicStrings = CardCrawlGame.languagePack.getRelicStrings(ID);
+    public static final String NAME = relicStrings.NAME;
+    public static final String[] DESCRIPTIONS = relicStrings.DESCRIPTIONS;
+    private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("Meadow-Plate.png"));
+    private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("Meadow-Plate.png"));
+
+    public GrassySeedRelic() {
+        super(ID, IMG, OUTLINE, RelicTier.COMMON, LandingSound.CLINK);
+
+        cardColor = Charmander.CARD_COLOR;
+        tips.clear();
+        tips.add(new PowerTip(name, description));
+    }
+ 
+    @Override
+    public void atBattleStartPreDraw() {
+        AbstractCard[] possibleCards = CardLibrary.cards.values().stream()
+                .filter(c -> c instanceof PokemonCard)
+                .filter(c -> c.color = Pokemon.partner.cardColor)
+                .filter(c -> c.color = Pokemon.adventurer.cardColor)
+                .filter(c -> c.color != CardColor.COLORLESS)
+                .filter(c -> !c.tags.contains(CardTags.STARTER_DEFEND))
+                .filter(c -> !c.tags.contains(CardTags.STARTER_STRIKE))
+                .toArray(AbstractCard[]::new);
+            AbstractCard c = possibleCards[(int)AbstractDungeon.cardRng.random(possibleCards.length-1)];
+            if (c.cost > 0) {
+                c.cost = 0;
+                c.costForTurn = 0;
+                c.isCostModified = true;
+              } 
+        addToBot(new MakeTempCardInDrawPileAction(c, 1, true, false, false));
+    }
+
+    // Description
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+
+}
