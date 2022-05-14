b44a97c45cc6df73c08de2d4af72443bcd26d91d
Ivel54 <abudjo55@gmail.com>
2022-05-14 19:59:27 -0400

    leaf guard nerf

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Chikorita/ChikoritaLeafGuard.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Chikorita/ChikoritaLeafGuard.java
@@ -34,7 +34,7 @@ public class ChikoritaLeafGuard extends PokemonCard {
     public static final CardColor COLOR = Pokemon.Enums.CHIKORITA_GREEN;
 
     private static final int COST = 1;
-    private static final int BASE_MAGIC_NUMBER = 2;
+    private static final int BASE_MAGIC_NUMBER = 4;
 
     // /STAT DECLARATION/
 
