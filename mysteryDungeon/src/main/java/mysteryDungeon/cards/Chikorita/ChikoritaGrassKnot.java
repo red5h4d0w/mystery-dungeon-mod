8311165926e2ce0f537406c207325bae4143d618
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2022-05-14 21:49:19 -0400

    iamge

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Chikorita/ChikoritaGrassKnot.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Chikorita/ChikoritaGrassKnot.java
@@ -21,7 +21,7 @@ public class ChikoritaGrassKnot extends PokemonCard {
 
     public static final String ID = MysteryDungeon.makeID(ChikoritaGrassKnot.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("ChikoritaAttack.png");
+    public static final String IMG = makeCardPath("ChikoritaSkill.png");
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 
