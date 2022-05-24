8e12836869afa3e1be8138527d79623552fcb527
Ivel54 <abudjo55@gmail.com>
2022-05-23 22:34:06 -0400

    tried to make an eruption action

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/actions/EruptionAction.java
@@ -0,0 +1,41 @@
+package mysteryDungeon.actions;
+
+
+import com.badlogic.gdx.math.MathUtils;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+
+import mysteryDungeon.powers.BurnPower;
+
+
+public class EruptionAction extends AbstractGameAction {
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("DiscardAction");
+    public static final String[] TEXT = uiStrings.TEXT;
+    private AbstractPlayer p;
+    private AbstractMonster m;
+
+
+    public EruptionAction() {
+        this.p = AbstractDungeon.player;
+        this.duration = Settings.ACTION_DUR_MED;
+    }
+    
+    public void update() {
+        if (!AbstractDungeon.getMonsters().areMonstersBasicallyDead() && this.m.currentHealth > 0)
+        {
+            int burnStacks = m.hasPower(BurnPower.POWER_ID)?m.getPower(BurnPower.POWER_ID).amount:0;
+            addToBot(new DamageAllEnemiesAction(null, DamageInfo.createDamageMatrix(MathUtils.floor(amount*burnStacks), true), DamageType.THORNS, AttackEffect.FIRE)); 
+            adToBot(new RemoveSpecificPowerAction(m, p, BurnPower(m, burnStacks)));
+        }
+    }
+}
