0db97414cfd858fdbc4921650a4977f4395165d2
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2022-05-29 16:13:28 -0400

    Update EruptionAction.java

--- mysteryDungeon/src/main/java/mysteryDungeon/actions/EruptionAction.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/actions/EruptionAction.java
@@ -1,11 +1,8 @@
 package mysteryDungeon.actions;
 
-
-import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -14,28 +11,25 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-
 import mysteryDungeon.powers.BurnPower;
-
 
 public class EruptionAction extends AbstractGameAction {
     private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("DiscardAction");
     public static final String[] TEXT = uiStrings.TEXT;
     private AbstractPlayer p;
-    private AbstractMonster m;
 
-
-    public EruptionAction() {
+    public EruptionAction(AbstractMonster target) {
+        setValues(target, source);
         this.p = AbstractDungeon.player;
         this.duration = Settings.ACTION_DUR_MED;
     }
-    
+
     public void update() {
-        if (!AbstractDungeon.getMonsters().areMonstersBasicallyDead() && this.m.currentHealth > 0)
-        {
-            int burnStacks = m.hasPower(BurnPower.POWER_ID)?m.getPower(BurnPower.POWER_ID).amount:0;
-            addToBot(new DamageAllEnemiesAction(null, DamageInfo.createDamageMatrix(MathUtils.floor(amount*burnStacks), true), DamageType.THORNS, AttackEffect.FIRE)); 
-            adToBot(new RemoveSpecificPowerAction(m, p, BurnPower(m, burnStacks)));
+        if (target.hasPower(BurnPower.POWER_ID)) {
+            int burnStacks = target.getPower(BurnPower.POWER_ID).amount;
+            addToBot(new DamageAllEnemiesAction(p, burnStacks, DamageType.THORNS, AttackEffect.FIRE));
+            addToBot(new RemoveSpecificPowerAction(target, p, target.getPower(BurnPower.POWER_ID)));
         }
+        isDone = true;
     }
 }
