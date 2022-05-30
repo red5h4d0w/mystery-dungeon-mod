8131ec6377e67646bca64edaa25447b02f45f276
Ivel54 <abudjo55@gmail.com>
2022-05-29 23:11:22 -0400

    found a whack soluce

--- mysteryDungeon/src/main/java/mysteryDungeon/actions/EruptionAction.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/actions/EruptionAction.java
@@ -1,11 +1,9 @@
 package mysteryDungeon.actions;
 
 
-import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.Settings;
@@ -18,20 +16,20 @@ import mysteryDungeon.powers.BurnPower;
 
 public class EruptionAction extends AbstractGameAction {
     private AbstractPlayer p;
-    private AbstractMonster m;
 
 
-    public EruptionAction() {
+    public EruptionAction(AbstractMonster target) {
+        setValues(target, source);
         this.p = AbstractDungeon.player;
         this.duration = Settings.ACTION_DUR_MED;
     }
     
     public void update() {
-        if (!AbstractDungeon.getMonsters().areMonstersBasicallyDead() && this.m.currentHealth > 0)
-        {
-            int burnStacks = m.hasPower(BurnPower.POWER_ID)?m.getPower(BurnPower.POWER_ID).amount:0;
-            addToBot(new DamageAllEnemiesAction(null, DamageInfo.createDamageMatrix(MathUtils.floor(amount*burnStacks), true), DamageType.THORNS, AttackEffect.FIRE)); 
-            adToBot(new RemoveSpecificPowerAction(m, p, BurnPower(m, burnStacks)));
+        if (target.hasPower(BurnPower.POWER_ID)) {
+            int burnStacks = target.getPower(BurnPower.POWER_ID).amount;
+            addToBot(new DamageAllEnemiesAction(p, burnStacks, DamageType.THORNS, AttackEffect.FIRE));
+            addToBot(new RemoveSpecificPowerAction(target, p, target.getPower(BurnPower.POWER_ID)));
         }
+        isDone = true;
     }
 }
