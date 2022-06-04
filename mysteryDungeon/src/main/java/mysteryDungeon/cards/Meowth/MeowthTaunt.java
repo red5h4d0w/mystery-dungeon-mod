46f9b3b05b5340e5e4e9dc39fe8702f8d6a1c4a3
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2022-06-04 17:41:23 -0400

    miaouss début

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthTaunt.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthTaunt.java
@@ -1,9 +1,8 @@
 package mysteryDungeon.cards.Meowth;
 
 import static mysteryDungeon.MysteryDungeon.makeCardPath;
-
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.actions.common.GainGoldAction;
+import com.megacrit.cardcrawl.actions.watcher.PressEndTurnButtonAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
@@ -13,11 +12,11 @@ import mysteryDungeon.MysteryDungeon;
 import mysteryDungeon.abstracts.PokemonCard;
 import mysteryDungeon.characters.Pokemon;
 
-public class MeowthDefend extends PokemonCard {
+public class MeowthTaunt extends PokemonCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = MysteryDungeon.makeID(MeowthDefend.class.getSimpleName());
+    public static final String ID = MysteryDungeon.makeID(MeowthTaunt.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String IMG = makeCardPath("Skill.png");
     public static final String NAME = cardStrings.NAME;
@@ -28,36 +27,37 @@ public class MeowthDefend extends PokemonCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.BASIC;
+    private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = Pokemon.Enums.MEOWTH_WHITE;
 
-    private static final int COST = 1;
-    private static final int BLOCK = 5;
-    private static final int UPGRADE_PLUS_BLOCK = 3;
+    private static final int COST = 2;
+    private static final int BASE_MAGIC_NUMBER = 25;
+    private static final int UPGRADE_MAGIC_NUMBER = 5;
 
 
     // /STAT DECLARATION/
 
-    public MeowthDefend() {
+    public MeowthTaunt() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-        baseBlock = BLOCK;
-        this.tags.add(AbstractCard.CardTags.STARTER_DEFEND);
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new GainBlockAction(p, p, block));
+        addToBot(new GainGoldAction(magicNumber));
+        addToBot(new PressEndTurnButtonAction());
     }
 
     // Upgraded stats.
     @Override
     public void upgrade() {
         if (!upgraded) {
+            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
             upgradeName();
-            upgradeBlock(UPGRADE_PLUS_BLOCK);
             initializeDescription();
         }
     }
