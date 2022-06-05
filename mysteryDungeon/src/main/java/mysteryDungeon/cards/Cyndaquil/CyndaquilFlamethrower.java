52c6ad5a96151ce0e235a63c6e98795760e092e9
Ivel54 <abudjo55@gmail.com>
2022-06-05 14:13:15 -0400

    cyndaquil flamethrower

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Cyndaquil/CyndaquilFlamethrower.java
@@ -0,0 +1,80 @@
+package mysteryDungeon.cards.Cyndaquil;
+
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonCard;
+import mysteryDungeon.characters.Pokemon;
+import mysteryDungeon.powers.BurnPower;
+
+public class CyndaquilFlamethrower extends PokemonCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(CyndaquilFlamethrower.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath(CyndaquilFlamethrower.class.getSimpleName()+".png");
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = Pokemon.Enums.CHARMANDER_RED;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 6;
+    private static final int UPGRADE_PLUS_DMG = 2;
+    private static final int BASE_MAGIC_NUMBER = 6;
+    private static final int UPGRADE_MAGIC_NUMBER = 2;
+
+
+    // /STAT DECLARATION/
+
+    public CyndaquilFlamethrower() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
+        isMultiDamage = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        // Create an int which equals to your current energy.
+        addToBot(new DamageAllEnemiesAction(p, multiDamage, damageTypeForTurn, AbstractGameAction.AttackEffect.FIRE));
+        if (!AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
+            for (AbstractMonster monster : (AbstractDungeon.getMonsters()).monsters) {
+                if (!monster.isDead && !monster.isDying) {
+                    addToBot(new ApplyPowerAction(monster, p, new BurnPower(monster, this.magicNumber), this.magicNumber));
+                } 
+            } 
+        }
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
+            initializeDescription();
+        }
+    }
+}
