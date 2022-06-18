5e74252d4748d6532e2eb5404d8d204921636bf5
Ivel54 <abudjo55@gmail.com>
2022-06-18 18:15:10 -0400

    lavaplume

--- mysteryDungeon/src/main/java/mysteryDungeon/actions/EruptionAction.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/actions/EruptionAction.java
@@ -2,6 +2,7 @@ package mysteryDungeon.actions;
 
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
@@ -15,6 +16,7 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import mysteryDungeon.powers.BurnPower;
 import mysteryDungeon.powers.InfernoPower;
+import mysteryDungeon.powers.LavaPlumePower;
 
 
 public class EruptionAction extends AbstractGameAction {
@@ -36,6 +38,9 @@ public class EruptionAction extends AbstractGameAction {
             }
             else {
                 addToBot(new RemoveSpecificPowerAction(target, p, target.getPower(BurnPower.POWER_ID))); 
+            }
+            if(AbstractDungeon.player.hasPower(LavaPlumePower.POWER_ID)){
+                addToBot(new ApplyPowerAction(target, source, new BurnPower(target,  target.getPower(LavaPlumePower.POWER_ID).amount)));
             }
         }
         isDone = true;
