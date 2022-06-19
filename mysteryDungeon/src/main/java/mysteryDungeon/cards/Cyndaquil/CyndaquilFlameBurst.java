40391fd3ae1762da86d284c903a8a932906a0528
Ivel54 <abudjo55@gmail.com>
2022-06-18 23:44:01 -0400

    Flame Burst

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Cyndaquil/CyndaquilFlameBurst.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Cyndaquil/CyndaquilFlameBurst.java
@@ -2,10 +2,8 @@ package mysteryDungeon.cards.Cyndaquil;
 
 import static mysteryDungeon.MysteryDungeon.makeCardPath;
 
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.actions.watcher.PressEndTurnButtonAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
@@ -16,13 +14,13 @@ import mysteryDungeon.abstracts.PokemonCard;
 import mysteryDungeon.actions.EruptionAction;
 import mysteryDungeon.characters.Pokemon;
 
-public class CyndaquilBlastBurn extends PokemonCard {
+public class CyndaquilFlameBurst extends PokemonCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = MysteryDungeon.makeID(CyndaquilBlastBurn.class.getSimpleName());
+    public static final String ID = MysteryDungeon.makeID(CyndaquilFlameBurst.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("Attack.png");
+    public static final String IMG = makeCardPath("Skill.png");
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 
@@ -33,25 +31,25 @@ public class CyndaquilBlastBurn extends PokemonCard {
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
-    private static final CardType TYPE = CardType.ATTACK;
+    private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = Pokemon.Enums.CYNDAQUIL_RED;
 
     private static final int COST = 2;
     private static final int UPGRADE_COST = 1;
-    private static final int DAMAGE = 13;
+    private static final int BLOCK = 9;
 
 
     // /STAT DECLARATION/
 
-    public CyndaquilBlastBurn() {
+    public CyndaquilFlameBurst() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-        baseDamage = DAMAGE;
+        baseBlock = BLOCK;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+        addToBot(new GainBlockAction(p, p, block));
         addToBot(new EruptionAction(m));
         addToBot(new PressEndTurnButtonAction());
         }
