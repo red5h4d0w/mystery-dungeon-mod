b97f3398b9a2f91218d7dcbc503b535a000027d8
Ivel54 <abudjo55@gmail.com>
2022-06-19 02:53:26 -0400

    buff counter+ real desc names

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Cyndaquil/CyndaquilCounter.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Cyndaquil/CyndaquilCounter.java
@@ -35,9 +35,9 @@ public class CyndaquilCounter extends PokemonCard {
     private static final CardType TYPE = CardType.POWER;
     public static final CardColor COLOR = Pokemon.Enums.CYNDAQUIL_RED;
 
-    private static final int COST = 2;
-    private static final int BASE_MAGIC_NUMBER = 2;
-    private static final int UPGRADE_MAGIC_NUMBER = 1;
+    private static final int COST = 1;
+    private static final int BASE_MAGIC_NUMBER = 6;
+    private static final int UPGRADE_MAGIC_NUMBER = 3;
 
 
     // /STAT DECLARATION/
