7b04c647e07a10a7ac2d6bab7c9156329db259dc
Ivel54 <abudjo55@gmail.com>
2022-06-22 20:24:10 -0400

    facade+ fling+ smokescreen

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Cyndaquil/CyndaquilFacade.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Cyndaquil/CyndaquilFacade.java
@@ -3,61 +3,60 @@ package mysteryDungeon.cards.Cyndaquil;
 import static mysteryDungeon.MysteryDungeon.makeCardPath;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.status.Burn;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DexterityPower;
 
 import mysteryDungeon.MysteryDungeon;
 import mysteryDungeon.abstracts.PokemonCard;
 import mysteryDungeon.characters.Pokemon;
-import mysteryDungeon.powers.BurnPower;
 
-public class CyndaquilFling extends PokemonCard {
+public class CyndaquilFacade extends PokemonCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = MysteryDungeon.makeID(CyndaquilFling.class.getSimpleName());
+    public static final String ID = MysteryDungeon.makeID(CyndaquilFacade.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("Skill.png");
+    public static final String IMG = makeCardPath("Power.png");
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
 
     // /TEXT DECLARATION/
 
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.COMMON;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
-    private static final CardType TYPE = CardType.SKILL;
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
     public static final CardColor COLOR = Pokemon.Enums.CYNDAQUIL_RED;
 
-    private static final int COST = 0;
-    private static final int BASE_MAGIC_NUMBER = 1;
+    private static final int COST = 1;
+    private static final int BASE_MAGIC_NUMBER = 2;
     private static final int UPGRADE_MAGIC_NUMBER = 1;
+
 
     // /STAT DECLARATION/
 
-    public CyndaquilFling() {
+    public CyndaquilFacade() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
         baseMagicNumber = BASE_MAGIC_NUMBER;
         magicNumber = baseMagicNumber;
-        exhaust = true;
-        
+        cardsToPreview = (AbstractCard)new Burn();
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new ApplyPowerAction(m, p, new BurnPower(m, 5)));
-        addToBot(new ApplyPowerAction(m, p, new BurnPower(m, 5)));
-        if(upgraded)
-        addToBot(new ApplyPowerAction(m, p, new BurnPower(m, 5)));
-        addToBot(new GainEnergyAction(magicNumber));
-        }
+        // Create an int which equals to your current energy.
+        addToBot(new MakeTempCardInDrawPileAction(new Burn(), 1, true, false, false));
+        addToBot(new ApplyPowerAction(p, p, new DexterityPower(p, magicNumber), magicNumber));
+    }
 
     // Upgraded stats.
     @Override
@@ -65,7 +64,6 @@ public class CyndaquilFling extends PokemonCard {
         if (!upgraded) {
             upgradeName();
             upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
-            rawDescription = UPGRADE_DESCRIPTION;
             initializeDescription();
         }
     }
--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Cyndaquil/CyndaquilSmokescreen.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Cyndaquil/CyndaquilSmokescreen.java
@@ -3,61 +3,57 @@ package mysteryDungeon.cards.Cyndaquil;
 import static mysteryDungeon.MysteryDungeon.makeCardPath;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.EnergizedPower;
 
 import mysteryDungeon.MysteryDungeon;
 import mysteryDungeon.abstracts.PokemonCard;
 import mysteryDungeon.characters.Pokemon;
-import mysteryDungeon.powers.BurnPower;
 
-public class CyndaquilFling extends PokemonCard {
+public class CyndaquilSmokescreen extends PokemonCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = MysteryDungeon.makeID(CyndaquilFling.class.getSimpleName());
+    public static final String ID = MysteryDungeon.makeID(CyndaquilSmokescreen.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String IMG = makeCardPath("Skill.png");
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
 
     // /TEXT DECLARATION/
 
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = Pokemon.Enums.CYNDAQUIL_RED;
 
-    private static final int COST = 0;
-    private static final int BASE_MAGIC_NUMBER = 1;
+    private static final int COST = 1;
+    private static final int BLOCK = 7;
+    private static final int BASE_MAGIC_NUMBER = 2;
     private static final int UPGRADE_MAGIC_NUMBER = 1;
 
     // /STAT DECLARATION/
 
-    public CyndaquilFling() {
+    public CyndaquilSmokescreen() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseBlock = BLOCK;
         baseMagicNumber = BASE_MAGIC_NUMBER;
         magicNumber = baseMagicNumber;
-        exhaust = true;
-        
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new ApplyPowerAction(m, p, new BurnPower(m, 5)));
-        addToBot(new ApplyPowerAction(m, p, new BurnPower(m, 5)));
-        if(upgraded)
-        addToBot(new ApplyPowerAction(m, p, new BurnPower(m, 5)));
-        addToBot(new GainEnergyAction(magicNumber));
-        }
+        addToBot(new ApplyPowerAction(m, p, new EnergizedPower(m, magicNumber)));
+        addToBot(new GainBlockAction(p, p, block));
+    }
 
     // Upgraded stats.
     @Override
@@ -65,7 +61,6 @@ public class CyndaquilFling extends PokemonCard {
         if (!upgraded) {
             upgradeName();
             upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
-            rawDescription = UPGRADE_DESCRIPTION;
             initializeDescription();
         }
     }
