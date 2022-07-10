f7165021f4540fbe79abfe02849537b22bf34275
Ivel54 <abudjo55@gmail.com>
2022-07-10 16:07:59 -0400

    smokescreen nerf

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Cyndaquil/CyndaquilSmokescreen.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Cyndaquil/CyndaquilSmokescreen.java
@@ -36,7 +36,7 @@ public class CyndaquilSmokescreen extends PokemonCard {
 
     private static final int COST = 1;
     private static final int BLOCK = 7;
-    private static final int BASE_MAGIC_NUMBER = 2;
+    private static final int BASE_MAGIC_NUMBER = 1;
     private static final int UPGRADE_MAGIC_NUMBER = 1;
 
     // /STAT DECLARATION/
