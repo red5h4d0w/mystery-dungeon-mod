76cba5cf9c4f0cd7de51b4a5638c0a204fb0243a
Ivel54 <abudjo55@gmail.com>
2022-08-31 14:12:23 -0400

    one more

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Cyndaquil/CyndaquilFirePledge.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Cyndaquil/CyndaquilFirePledge.java
@@ -37,7 +37,7 @@ public class CyndaquilFirePledge extends PokemonCard {
     public static final CardColor COLOR = Pokemon.Enums.CYNDAQUIL_RED;
 
     private static final int COST = 1;
-    private static final int BASE_MAGIC_NUMBER = 15;
+    private static final int BASE_MAGIC_NUMBER = 12;
     private static final int UPGRADE_MAGIC_NUMBER = 3;
 
     // /STAT DECLARATION/
