a20492731bc5c2ad402d44938768bb427bf41c76
Ivel54 <abudjo55@gmail.com>
2022-09-03 13:18:28 -0400

    Blast Burn rework

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Cyndaquil/CyndaquilBlastBurn.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Cyndaquil/CyndaquilBlastBurn.java
@@ -3,6 +3,7 @@ package mysteryDungeon.cards.Cyndaquil;
 import static mysteryDungeon.MysteryDungeon.makeCardPath;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.watcher.PressEndTurnButtonAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
@@ -15,6 +16,7 @@ import mysteryDungeon.MysteryDungeon;
 import mysteryDungeon.abstracts.PokemonCard;
 import mysteryDungeon.actions.EruptionAction;
 import mysteryDungeon.characters.Pokemon;
+import mysteryDungeon.powers.BurnPower;
 
 public class CyndaquilBlastBurn extends PokemonCard {
 
@@ -37,8 +39,10 @@ public class CyndaquilBlastBurn extends PokemonCard {
     public static final CardColor COLOR = Pokemon.Enums.CYNDAQUIL_RED;
 
     private static final int COST = 2;
-    private static final int UPGRADE_COST = 1;
-    private static final int DAMAGE = 12;
+    private static final int DAMAGE = 10;
+    private static final int UPGRADE_PLUS_DMG = 2;
+    private static final int BASE_MAGIC_NUMBER = 10;
+    private static final int UPGRADE_MAGIC_NUMBER = 2;
 
 
     // /STAT DECLARATION/
@@ -46,12 +50,15 @@ public class CyndaquilBlastBurn extends PokemonCard {
     public CyndaquilBlastBurn() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
         baseDamage = DAMAGE;
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+        addToBot(new ApplyPowerAction(m, p, new BurnPower(m, magicNumber)));
         addToBot(new EruptionAction(m));
         addToBot(new PressEndTurnButtonAction());
         }
@@ -61,7 +68,8 @@ public class CyndaquilBlastBurn extends PokemonCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeBaseCost(UPGRADE_COST);
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
             initializeDescription();
         }
     }
