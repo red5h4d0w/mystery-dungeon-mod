efb1c3fe1e1d86a11f22fb10a5039a6705326137
Ivel54 <abudjo55@gmail.com>
2022-10-03 16:48:50 -0400

    feint

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthFeint.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthFeint.java
@@ -30,7 +30,7 @@ public class MeowthFeint extends PokemonCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = Pokemon.Enums.MEOWTH_WHITE;
