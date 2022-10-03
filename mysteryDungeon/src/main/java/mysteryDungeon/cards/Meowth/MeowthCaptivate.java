ba444c7f0959ba043c373702801aa69791e6ed0a
Ivel54 <abudjo55@gmail.com>
2022-10-03 16:08:27 -0400

    meowth captivate

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthCaptivate.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthCaptivate.java
@@ -32,7 +32,7 @@ public class MeowthCaptivate extends PokemonCard {
     // STAT DECLARATION
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
-    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = Pokemon.Enums.MEOWTH_WHITE;
 
