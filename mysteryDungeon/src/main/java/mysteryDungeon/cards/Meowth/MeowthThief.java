2292019d7a69240f9c4e2d5246fcf624145bf1eb
Ivel54 <71346308+Ivel54@users.noreply.github.com>
2022-10-06 14:46:24 -0400

    new Thief

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthThief.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthThief.java
@@ -2,6 +2,8 @@ package mysteryDungeon.cards.Meowth;
 
 import static mysteryDungeon.MysteryDungeon.makeCardPath;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction;
 import com.megacrit.cardcrawl.actions.common.GainGoldAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -18,7 +20,7 @@ public class MeowthThief extends PokemonCard {
 
     public static final String ID = MysteryDungeon.makeID(MeowthThief.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("Skill.png");
+    public static final String IMG = makeCardPath("Attack.png");
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 
@@ -29,27 +31,31 @@ public class MeowthThief extends PokemonCard {
 
     private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
-    private static final CardType TYPE = CardType.SKILL;
+    private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = Pokemon.Enums.MEOWTH_WHITE;
 
     private static final int COST = 1;
-    private static final int BASE_MAGIC_NUMBER = 10;
-    private static final int UPGRADE_MAGIC_NUMBER = 4;
+    private static final int DAMAGE = 4;
+    private static final int BASE_MAGIC_NUMBER = 2;
+    private static final int UPGRADE_MAGIC_NUMBER = 1;
 
 
     // /STAT DECLARATION/
 
     public MeowthThief() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
         baseMagicNumber = BASE_MAGIC_NUMBER;
         magicNumber = baseMagicNumber;
-        exhaust = true;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new GainGoldAction(magicNumber));
+        for (int i = 0; i < this.magicNumber; i++){
+            addToBot(new AttackDamageRandomEnemyAction(this, AttackEffect.SLASH_HORIZONTAL));
+            addToBot(new GainGoldAction(1));
+            }
     }
 
     // Upgraded stats.
