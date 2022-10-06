200a6f2d1fd8071bcc7d1de9133262cbd4bbf768
Ivel54 <71346308+Ivel54@users.noreply.github.com>
2022-10-06 15:15:29 -0400

    Assurance

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthAssurance.java
@@ -0,0 +1,77 @@
+package mysteryDungeon.cards.Meowth;
+
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainGoldAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.WeakPower;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonCard;
+import mysteryDungeon.actions.SpendGoldAction;
+import mysteryDungeon.characters.Pokemon;
+
+public class MeowthAssurance extends PokemonCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(MeowthAssurance.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath("Skill.png");
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Pokemon.Enums.MEOWTH_WHITE;
+
+    private static final int COST = 0;
+    private static final int BASE_MAGIC_NUMBER = 3;
+    private static final int UPGRADE_MAGIC_NUMBER = 5;
+
+    // /STAT DECLARATION/
+
+    public MeowthAssurance() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
+        exhaust = true;
+        isInnate = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot (new SpendGoldAction(3));
+        addToBot (new ApplyPowerAction(p, m, new WeakPower(m, 1 , false)));
+        if(AbstractDungeon.actionManager.cardsPlayedThisTurn.size() < 1)
+            addToBot (new GainGoldAction(magicNumber));
+
+    }
+    
+      public void triggerOnGlowCheck() {
+        this.glowColor = (AbstractDungeon.actionManager.cardsPlayedThisCombat.size() < 1) ? AbstractCard.GOLD_BORDER_GLOW_COLOR : AbstractCard.BLUE_BORDER_GLOW_COLOR;
+      }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
+            initializeDescription();
+        }
+    }
+}
