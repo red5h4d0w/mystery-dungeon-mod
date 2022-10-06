8a148b5f7b67be6c28463bfcf89e5bb2de9251be
Ivel54 <71346308+Ivel54@users.noreply.github.com>
2022-10-06 15:24:16 -0400

    Amnesia

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthAmnesia.java
@@ -0,0 +1,68 @@
+package mysteryDungeon.cards.Meowth;
+
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+
+import com.jcraft.jogg.Buffer;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.BufferPower;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonCard;
+import mysteryDungeon.actions.SpendGoldAction;
+import mysteryDungeon.characters.Pokemon;
+
+public class MeowthAmnesia extends PokemonCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(MeowthAmnesia.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath("Power.png");
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = Pokemon.Enums.MEOWTH_WHITE;
+
+    private static final int COST = 2;
+    private static final int BASE_MAGIC_NUMBER = 1;
+    private static final int UPGRADE_MAGIC_NUMBER = 1;
+
+
+    // /STAT DECLARATION/
+
+    public MeowthAmnesia() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new SpendGoldAction(15));
+        addToBot(new ApplyPowerAction(p, p, new BufferPower(p, magicNumber)));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
+            upgradeName();
+            initializeDescription();
+        }
+    }
+}
