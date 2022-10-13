279b5027875c2c258bd0b133a8fd4b40ff11810d
Ivel54 <71346308+Ivel54@users.noreply.github.com>
2022-10-13 14:42:55 -0400

    Meowth Flatter

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthFlatter.java
@@ -0,0 +1,77 @@
+package mysteryDungeon.cards.Meowth;
+
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonCard;
+import mysteryDungeon.characters.Pokemon;
+
+public class MeowthFlatter extends PokemonCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(MeowthFlatter.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath("Skill.png");
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Pokemon.Enums.MEOWTH_WHITE;
+
+    private static final int COST = 2;
+    private static final int BASE_MAGIC_NUMBER = 1;
+    private static final int BLOCK = 20;
+    private static final int UPGRADE_PLUS_BLOCK = 5;
+
+
+    // /STAT DECLARATION/
+
+    public MeowthFlatter() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseBlock = BLOCK;
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (!AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
+            for (AbstractMonster monster : (AbstractDungeon.getCurrRoom()).monsters.monsters) {
+                if (!monster.isDead && !monster.isDying) {
+                    AbstractDungeon.actionManager.addToBottom(
+                        new ApplyPowerAction(monster, p, new StrengthPower(monster, this.magicNumber), this.magicNumber)
+                    );
+                } 
+            } 
+        }
+        addToBot(new GainBlockAction(p, p, block));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_PLUS_BLOCK);
+            initializeDescription();
+        }
+    }
+}
