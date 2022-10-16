79c3204b11647d8a65ea8a7cc9e6e99577b111bc
Ivel54 <abudjo55@gmail.com>
2022-10-16 15:02:50 -0400

    Screech

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthScreech.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthScreech.java
@@ -3,24 +3,22 @@ package mysteryDungeon.cards.Meowth;
 import static mysteryDungeon.MysteryDungeon.makeCardPath;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.ArtifactPower;
-import com.megacrit.cardcrawl.powers.GainStrengthPower;
-import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
 
 import mysteryDungeon.MysteryDungeon;
 import mysteryDungeon.abstracts.PokemonCard;
-import mysteryDungeon.actions.SpendGoldAction;
 import mysteryDungeon.characters.Pokemon;
 
-public class MeowthCaptivate extends PokemonCard {
+public class MeowthScreech extends PokemonCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = MysteryDungeon.makeID(MeowthCaptivate.class.getSimpleName());
+    public static final String ID = MysteryDungeon.makeID(MeowthScreech.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String IMG = makeCardPath("MeowthSkill.png");
     public static final String NAME = cardStrings.NAME;
@@ -31,48 +29,39 @@ public class MeowthCaptivate extends PokemonCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = Pokemon.Enums.MEOWTH_WHITE;
 
     private static final int COST = 0;
-    private static final int BASE_MAGIC_NUMBER = 15;
-    private static final int BASE_SECOND_MAGIC_NUMBER = 7;
-    private static final int UPGRADE_SECOND_MAGIC_NUMBER = 2;
+    private static final int BASE_MAGIC_NUMBER = 2;
+    private static final int UPGRADE_MAGIC_NUMBER = 2;
+    private static final int BLOCK = 13;
 
 
     // /STAT DECLARATION/
 
-    public MeowthCaptivate() {
+    public MeowthScreech() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
         baseMagicNumber = BASE_MAGIC_NUMBER;
         magicNumber = baseMagicNumber;
-        baseSecondMagicNumber = BASE_SECOND_MAGIC_NUMBER;
-        secondMagicNumber = baseSecondMagicNumber;
+        baseBlock = BLOCK;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new SpendGoldAction(magicNumber));
-        addToBot(new ApplyPowerAction(m, p, new StrengthPower(m, -this.secondMagicNumber), -this.secondMagicNumber));
-        if (m != null && !m.hasPower(ArtifactPower.POWER_ID))
-            addToBot(new ApplyPowerAction(m, p, new GainStrengthPower(m, this.secondMagicNumber), this.secondMagicNumber)); 
-      
-    }
+        addToBot(new ApplyPowerAction(m, p, new VulnerablePower(m, magicNumber, false)));
+        addToBot(new GainBlockAction(m, p, block));
 
-    @Override
-    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
-        return super.canUse(p, m) && canSpend(magicNumber);
     }
-
 
     // Upgraded stats.
     @Override
     public void upgrade() {
         if (!upgraded) {
-            upgradeSecondMagicNumber(UPGRADE_SECOND_MAGIC_NUMBER);
+            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
             upgradeName();
             initializeDescription();
         }
