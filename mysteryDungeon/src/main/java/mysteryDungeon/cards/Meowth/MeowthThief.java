5ea6b0d39613c5ba10e7030499af14ae26fcd5a0
Ivel54 <abudjo55@gmail.com>
2022-10-16 13:48:44 -0400

    thief buff + strengTH

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthThief.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthThief.java
@@ -36,7 +36,7 @@ public class MeowthThief extends PokemonCard {
 
     private static final int COST = 1;
     private static final int DAMAGE = 6;
-    private static final int BASE_MAGIC_NUMBER = 2;
+    private static final int BASE_MAGIC_NUMBER = 4;
     private static final int UPGRADE_MAGIC_NUMBER = 1;
 
 
