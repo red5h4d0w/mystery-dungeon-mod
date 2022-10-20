d12d44b87d14624021df41f1ad8f01071dc39d19
Ivel54 <71346308+Ivel54@users.noreply.github.com>
2022-10-20 14:41:41 -0400

    New Hone claws + Last Resort

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthLastResort.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthLastResort.java
@@ -2,67 +2,74 @@ package mysteryDungeon.cards.Meowth;
 
 import static mysteryDungeon.MysteryDungeon.makeCardPath;
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.StrengthPower;
 
 import mysteryDungeon.MysteryDungeon;
 import mysteryDungeon.abstracts.PokemonCard;
+import mysteryDungeon.actions.SpendGoldAction;
 import mysteryDungeon.characters.Pokemon;
 
-public class MeowthHoneClaws extends PokemonCard {
+public class MeowthLastResort extends PokemonCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = MysteryDungeon.makeID(MeowthHoneClaws.class.getSimpleName());
+    public static final String ID = MysteryDungeon.makeID(MeowthLastResort.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String IMG = makeCardPath("MeowthSkill.png");
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
 
     // /TEXT DECLARATION/
 
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.UNCOMMON;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = Pokemon.Enums.MEOWTH_WHITE;
 
     private static final int COST = 0;
-    private static final int BLOCK = 13;
-    private static final int BASE_MAGIC_NUMBER = 1;
-    private static final int UPGRADE_MAGIC_NUMBER = 1;
+    private static final int BASE_MAGIC_NUMBER = 50;
+    private static final int UPGRADE_MAGIC_NUMBER = -10;
 
 
     // /STAT DECLARATION/
 
-    public MeowthHoneClaws() {
+    public MeowthLastResort() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-        baseBlock = BLOCK;
         baseMagicNumber = BASE_MAGIC_NUMBER;
-        magicNumber = baseMagicNumber;
+        magicNumber = BASE_MAGIC_NUMBER;
+        exhaust = true;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new GainBlockAction(m, p, block));
-        addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, magicNumber)));
+       addToBot(new SpendGoldAction(magicNumber));
+       addToBot(new DiscardAction(p, p, 10, true));
+       addToBot(new DrawCardAction(p, 10));
     }
 
+    @Override
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        return super.canUse(p, m) && canSpend(magicNumber);
+    }
 
     // Upgraded stats.
     @Override
     public void upgrade() {
         if (!upgraded) {
-            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
             upgradeName();
+            selfRetain = true;
+            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
+            rawDescription = UPGRADE_DESCRIPTION;
             initializeDescription();
         }
     }
