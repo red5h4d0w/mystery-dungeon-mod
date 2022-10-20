c8a601224939aec2d4e2688e6a86499debef29f5
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2022-10-20 17:10:05 -0400

    remove loggers

--- mysteryDungeon/src/main/java/mysteryDungeon/patches/EvolutionPatch.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/patches/EvolutionPatch.java
@@ -1,8 +1,5 @@
 package mysteryDungeon.patches;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -11,8 +8,6 @@ import mysteryDungeon.characters.Pokemon;
 
 @SpirePatch2(clz = AbstractDungeon.class, method = "generateMap")
 public class EvolutionPatch {
-
-    public static Logger logger = LogManager.getLogger(EvolutionPatch.class);
 
     @SpirePostfixPatch
     public static void EvolvePokemon() {
