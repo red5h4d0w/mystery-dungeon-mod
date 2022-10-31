2325978b9e2fea213f6a464037e757dfb563da58
Ivel54 <71346308+Ivel54@users.noreply.github.com>
2022-10-31 15:17:06 -0400

    Switcheroo rarity modified

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthSwitcheroo.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthSwitcheroo.java
@@ -30,7 +30,7 @@ public class MeowthSwitcheroo extends PokemonCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.RARE;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = Pokemon.Enums.MEOWTH_WHITE;
