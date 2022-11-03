4c933a68169f8d8d596ce62b45241f2275ef5c6a
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2022-11-03 13:16:26 -0400

    Update LashOutPower.java

--- mysteryDungeon/src/main/java/mysteryDungeon/powers/LashOutPower.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/powers/LashOutPower.java
@@ -57,14 +57,25 @@ public class LashOutPower extends PokemonPower implements CloneablePowerInterfac
     }
 
     @Override
-    public int betterOnApplyPowerStacks(com.megacrit.cardcrawl.powers.AbstractPower power, com.megacrit.cardcrawl.core.AbstractCreature target, com.megacrit.cardcrawl.core.AbstractCreature source, int stackAmount) {
-        if(source == owner && target!=owner && power instanceof StrengthPower && stackAmount >= 0) {
+    public boolean betterOnApplyPower(AbstractPower power, AbstractCreature target, AbstractCreature source) {
+        if(source == owner && target!=owner && power instanceof StrengthPower && power.amount>0) {
             AbstractCard flurry = new MeowthFlurry();
             if(upgraded)
                 flurry.upgrade();
             addToBot((AbstractGameAction)new MakeTempCardInHandAction(new MeowthFlurry(), amount, false));
         }
-        return 0;
+        return true;
+    }
+
+    @Override
+    public int betterOnApplyPowerStacks(AbstractPower power, AbstractCreature target, AbstractCreature source, int stackAmount) {
+        if(source == owner && target!=owner && power instanceof StrengthPower && stackAmount > 0) {
+            AbstractCard flurry = new MeowthFlurry();
+            if(upgraded)
+                flurry.upgrade();
+            addToBot((AbstractGameAction)new MakeTempCardInHandAction(new MeowthFlurry(), amount, false));
+        }
+        return stackAmount;
     }
 
     @Override
