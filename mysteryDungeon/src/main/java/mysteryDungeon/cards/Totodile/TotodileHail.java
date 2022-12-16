974e6d4d1e138c7f212067251cfa7b0cef8221e6
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2022-12-15 20:30:46 -0500

    Update TotodileHail.java

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Totodile/TotodileHail.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Totodile/TotodileHail.java
@@ -3,6 +3,7 @@ package mysteryDungeon.cards.Totodile;
 import static mysteryDungeon.MysteryDungeon.makeCardPath;
 
 import com.megacrit.cardcrawl.actions.defect.ChannelAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
@@ -58,6 +59,11 @@ public class TotodileHail extends PokemonCard {
     public void triggerOnManualDiscard() {
         super.triggerOnManualDiscard();
         addToBot(new ChannelAction(new Frost()));
+    }
+
+    @Override
+    public boolean canPlay(AbstractCard card) {
+        return super.canPlay(card) && false;
     }
 
     // Upgraded stats.
