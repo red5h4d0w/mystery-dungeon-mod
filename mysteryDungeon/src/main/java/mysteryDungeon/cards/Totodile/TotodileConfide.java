d707593f3687ccb871f8d9de5fa11eb54b5bb5bf
Ivel54 <71346308+Ivel54@users.noreply.github.com>
2022-12-16 01:14:19 -0500

    Confide

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Totodile/TotodileConfide.java
@@ -0,0 +1,70 @@
+package mysteryDungeon.cards.Totodile;
+
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.watcher.VigorPower;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonCard;
+import mysteryDungeon.characters.Pokemon;
+
+public class TotodileConfide extends PokemonCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(TotodileConfide.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath("TotodileSkill.png");
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Pokemon.Enums.TOTODILE_BLUE;
+
+    private static final int COST = 1;    
+    private static final int BASE_MAGIC_NUMBER = 1;
+    private static final int UPGRADE_MAGIC_NUMBER = 1;
+    private static final int BASE_SECOND_MAGIC_NUMBER = 7;
+    private static final int UPGRADE_SECOND_MAGIC_NUMBER = 3;
+
+    // /STAT DECLARATION/
+
+    public TotodileConfide() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
+        baseSecondMagicNumber = BASE_SECOND_MAGIC_NUMBER;
+        secondMagicNumber = baseSecondMagicNumber;
+        exhaust = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(m, p, new VulnerablePower(m, magicNumber, false)));
+        addToBot(new ApplyPowerAction(p, p, new VigorPower(p, secondMagicNumber)));
+        }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
+            upgradeSecondMagicNumber(UPGRADE_SECOND_MAGIC_NUMBER);            
+            initializeDescription();
+        }
+    }
+}
