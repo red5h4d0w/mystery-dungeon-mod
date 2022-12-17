a5ce628827f492a2e60cabf0b4825f73bee1117c
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2022-12-17 17:03:06 -0500

    Draft: Sheer Force

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/powers/TotodileSheerForcePower.java
@@ -0,0 +1,79 @@
+package mysteryDungeon.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonPower;
+import mysteryDungeon.util.TextureLoader;
+
+import static mysteryDungeon.MysteryDungeon.makePowerPath;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.defect.ChannelAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.orbs.Frost;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+
+//Gain 1 dex for the turn for each card played.
+
+public class TotodileSheerForce extends PokemonPower implements CloneablePowerInterface {
+    public AbstractCreature source;
+
+    public static final String POWER_ID = MysteryDungeon.makeID(TotodileSheerForce.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    private int limit;
+    
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath(TotodileSheerForce.class.getSimpleName()+"84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath(TotodileSheerForce.class.getSimpleName()+"32.png"));
+
+    public TotodileSheerForce(final AbstractCreature owner, final int amount, final int limit) {
+        name = NAME;
+        ID = POWER_ID;
+
+        this.owner = owner;
+        this.amount = amount;
+        this.limit = limit;
+
+        type = PowerType.BUFF;
+
+        // We load those txtures here.
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    @Override
+    public void onAttack(DamageInfo info, int damageAmount, AbstractCreature target) {
+        super.onAttack(info, damageAmount, target);
+        if(damageAmount > limit) {
+            addToBot(new ChannelAction(new Frost()));
+        }
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new TotodileSheerForce(owner, amount, limit);
+    }
+
+    @Override
+    public void updateDescription() {
+        if(amount == 1)
+        {
+            description = String.format(DESCRIPTIONS[0], limit);
+        } 
+        else
+        {
+            description = String.format(DESCRIPTIONS[1], limit, amount);
+        }
+    }
+}
