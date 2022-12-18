aae71321b3b5bd0388ed676750e0be6656ead941
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2022-12-18 11:43:44 -0500

    naive explorer

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/relics/NaiveExplorerRelic.java
@@ -0,0 +1,65 @@
+package mysteryDungeon.relics;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonRelic;
+import mysteryDungeon.util.TextureLoader;
+
+import static mysteryDungeon.MysteryDungeon.makeRelicOutlinePath;
+import static mysteryDungeon.MysteryDungeon.makeRelicPath;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.RelicStrings;
+import com.megacrit.cardcrawl.powers.watcher.VigorPower;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+
+public class NaiveExplorerRelic extends PokemonRelic { 
+
+    // ID, images, text.
+    public static final String ID = MysteryDungeon.makeID(NaiveExplorerRelic.class.getSimpleName());
+
+    private static final RelicStrings relicStrings = CardCrawlGame.languagePack.getRelicStrings(ID);
+    public static final String NAME = relicStrings.NAME;
+    public static final String[] DESCRIPTIONS = relicStrings.DESCRIPTIONS;
+    private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("band9.png"));
+    private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("default_clickable_relic.png"));
+
+    private boolean works; 
+
+    public NaiveExplorerRelic() {
+        super(ID, IMG, OUTLINE, RelicTier.STARTER, LandingSound.CLINK);
+    }
+
+    @Override
+    public void atTurnStart() {
+        if(works)
+        {
+            flash();
+            addToBot(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new VigorPower(AbstractDungeon.player, 3), 3));
+        }
+    }
+
+    @Override
+    public void wasHPLost(int damageAmount) {
+        super.wasHPLost(damageAmount);
+        if ((AbstractDungeon.getCurrRoom()).phase == AbstractRoom.RoomPhase.COMBAT && 
+            damageAmount > 0) {
+            works = false;
+        } 
+    }
+
+    @Override
+    public void atPreBattle()
+    {
+        works = true;
+    }
+
+    // Description
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+
+}
