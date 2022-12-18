7273b437acb164bb5b5f1443168263c122a01734
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2022-12-17 23:50:14 -0500

    Update TotodileBlizzard.java

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Totodile/TotodileBlizzard.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Totodile/TotodileBlizzard.java
@@ -31,7 +31,7 @@ public class TotodileBlizzard extends PokemonCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.RARE;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = Pokemon.Enums.TOTODILE_BLUE;
