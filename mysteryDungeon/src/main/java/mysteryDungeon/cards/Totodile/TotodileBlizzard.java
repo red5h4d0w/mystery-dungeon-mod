031d39afa4bae806072bb875f9fcf3cb72d6479b
Ivel54 <abudjo55@gmail.com>
2022-12-19 09:09:06 -0500

    bllizzard+ was busted

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Totodile/TotodileBlizzard.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Totodile/TotodileBlizzard.java
@@ -40,8 +40,8 @@ public class TotodileBlizzard extends PokemonCard {
     public static final CardColor COLOR = Pokemon.Enums.TOTODILE_BLUE;
 
     private static final int COST = 3;
-    private static final int BASE_MAGIC_NUMBER = 4;
-    private static final int UPGRADE_MAGIC_NUMBER = 6;
+    private static final int BASE_MAGIC_NUMBER = 5;
+    private static final int UPGRADE_MAGIC_NUMBER = 1;
 
 
     // /STAT DECLARATION/
