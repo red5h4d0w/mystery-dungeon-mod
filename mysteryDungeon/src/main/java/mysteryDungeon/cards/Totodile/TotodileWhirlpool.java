1874ae2fc011d022e58c360deb28d07792495c2b
Ivel54 <abudjo55@gmail.com>
2022-12-30 15:15:22 -0500

    Endure and whirlpool

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Totodile/TotodileWhirlpool.java
@@ -0,0 +1,69 @@
+package mysteryDungeon.cards.Totodile;
+
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.FocusPower;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonCard;
+import mysteryDungeon.actions.DiscardTopOfDrawPileAction;
+import mysteryDungeon.characters.Pokemon;
+
+public class TotodileWhirlpool extends PokemonCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(TotodileWhirlpool.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath("TotodileSkill.png");
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Pokemon.Enums.TOTODILE_BLUE;
+
+    private static final int COST = 1;
+    private static final int BASE_MAGIC_NUMBER = 2;
+    private static final int UPGRADE_MAGIC_NUMBER = 2;
+    private static final int BASE_SECOND_MAGIC_NUMBER = 1;
+    private static final int UPGRADE_SECOND_MAGIC_NUMBER = 1;
+
+    // /STAT DECLARATION
+
+    public TotodileWhirlpool() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
+        baseSecondMagicNumber = BASE_SECOND_MAGIC_NUMBER;
+        secondMagicNumber = baseSecondMagicNumber;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DiscardTopOfDrawPileAction(magicNumber));
+        addToBot(new ApplyPowerAction(p, p, new FocusPower(p, secondMagicNumber)));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
+            upgradeSecondMagicNumber(UPGRADE_SECOND_MAGIC_NUMBER);
+            initializeDescription();
+        }
+    }
+}
---
+++ mysteryDungeon/src/main/java/mysteryDungeon/powers/TotodileEndurePower.java
@@ -0,0 +1,75 @@
+package mysteryDungeon.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonPower;
+import mysteryDungeon.util.TextureLoader;
+
+import static mysteryDungeon.MysteryDungeon.makePowerPath;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.defect.ChannelAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.orbs.Frost;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+
+//Gain 1 dex for the turn for each card played.
+
+public class TotodileEndurePower extends PokemonPower implements CloneablePowerInterface {
+    public AbstractCreature source;
+
+    public static final String POWER_ID = MysteryDungeon.makeID(TotodileEndurePower.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath(TotodileEndurePower.class.getSimpleName()+"84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath(TotodileEndurePower.class.getSimpleName()+"32.png"));
+
+    public TotodileEndurePower(final AbstractCreature owner, final int amount) {
+        name = NAME;
+        ID = POWER_ID;
+
+        this.owner = owner;
+        this.amount = amount;
+
+        type = PowerType.BUFF;
+
+        // We load those txtures here.
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    @Override
+    public int onAttacked(DamageInfo info, int damageAmount) {
+        if (damageAmount > 0 && info.type == DamageType.NORMAL)
+            addToBot((AbstractGameAction)new ChannelAction(new Frost()));
+        return damageAmount;
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new TotodileEndurePower(owner, amount);
+    }
+
+    @Override
+    public void updateDescription() {
+        if(amount==1)
+            description = String.format(DESCRIPTIONS[0], amount);
+        else
+            description = String.format(DESCRIPTIONS[1], amount);
+    }
+}
+
