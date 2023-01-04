2bd9202ac9be0ab487395138eef65ed4ae7cb721
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2023-01-04 17:09:39 -0500

    totodile thrash

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Totodile/TotodileThrash.java
@@ -0,0 +1,75 @@
+package mysteryDungeon.cards.Totodile;
+
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonCard;
+import mysteryDungeon.characters.Pokemon;
+import mysteryDungeon.powers.ThrashPlusPower;
+import mysteryDungeon.powers.ThrashPower;
+
+public class TotodileThrash extends PokemonCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(TotodileThrash.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath("TotodilePower.png");
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = Pokemon.Enums.TOTODILE_BLUE;
+
+    private static final int COST = 1;
+
+    private static final int BASE_MAGIC_NUMBER = 4;
+    private static final int UPGRADE_MAGIC_NUMBER = -1;
+    private static final int BASE_SECOND_MAGIC_NUMBER = 6;
+
+    // /STAT DECLARATION/
+
+    public TotodileThrash() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
+        baseSecondMagicNumber = BASE_SECOND_MAGIC_NUMBER;
+        secondMagicNumber = baseSecondMagicNumber;
+        isEthereal = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if(upgraded) {
+            addToBot(new ApplyPowerAction(p, p, new ThrashPlusPower(p, secondMagicNumber), secondMagicNumber));
+        }
+        else {
+            addToBot(new ApplyPowerAction(p, p, new ThrashPower(p, secondMagicNumber), secondMagicNumber));
+        }
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
+            initializeDescription();
+        }
+    }
+}
