a152b719d3d8213e9447453d9b48e6df013dbe85
Ivel54 <abudjo55@gmail.com>
2023-01-04 04:29:51 -0500

    Strength?

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/actions/StrengthAction.java
@@ -0,0 +1,25 @@
+package mysteryDungeon.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.defect.ChannelAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.orbs.Frost;
+
+public class StrengthAction extends AbstractGameAction {
+  
+    public StrengthAction(AbstractCreature target, AbstractCreature source) {
+        this.target = target;
+        this.source = source;
+    }
+  
+    public void update() {
+        for (AbstractCard c : DrawCardAction.drawnCards) {
+            if (c.type == AbstractCard.CardType.ATTACK) {
+                addToBot(new ChannelAction(new Frost()));
+            } 
+        } 
+        isDone = true;
+    }
+}
---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Totodile/TotodileStrength.java
@@ -0,0 +1,64 @@
+package mysteryDungeon.cards.Totodile;
+
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonCard;
+import mysteryDungeon.characters.Pokemon;
+
+public class TotodileStrength extends PokemonCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(TotodileStrength.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath("TotodileSkill.png");
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Pokemon.Enums.TOTODILE_BLUE;
+
+    private static final int COST = 1;    
+    private static final int UPGRADE_COST = 0;
+    private static final int BASE_MAGIC_NUMBER = 1;
+
+    // /STAT DECLARATION/
+
+    public TotodileStrength() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
+        showEvokeValue = true;
+        showEvokeOrbCount = 1;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DrawCardAction(magicNumber));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();            
+            upgradeBaseCost(UPGRADE_COST);
+            initializeDescription();
+        }
+    }
+}
