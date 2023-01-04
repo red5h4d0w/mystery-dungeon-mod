e4386434aa830a748a7480bd42045cf98e048907
Ivel54 <abudjo55@gmail.com>
2023-01-04 14:05:30 -0500

    whirlpool nerf

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Totodile/TotodileWhirlpool.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Totodile/TotodileWhirlpool.java
@@ -34,8 +34,8 @@ public class TotodileWhirlpool extends PokemonCard {
     public static final CardColor COLOR = Pokemon.Enums.TOTODILE_BLUE;
 
     private static final int COST = 1;
-    private static final int BASE_MAGIC_NUMBER = 2;
-    private static final int UPGRADE_MAGIC_NUMBER = 2;
+    private static final int BASE_MAGIC_NUMBER = 3;
+    private static final int UPGRADE_MAGIC_NUMBER = 3;
     private static final int BASE_SECOND_MAGIC_NUMBER = 1;
     private static final int UPGRADE_SECOND_MAGIC_NUMBER = 1;
 
