853f7cfe3a1f0be7a37e56933f9380838a98827f
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2023-01-04 17:17:23 -0500

    waataa paarusu

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Totodile/TotodileWaterPulse.java
@@ -0,0 +1,71 @@
+package mysteryDungeon.cards.Totodile;
+
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonCard;
+import mysteryDungeon.actions.DiscardTopOfDrawPileAction;
+import mysteryDungeon.characters.Pokemon;
+
+public class TotodileWaterPulse extends PokemonCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(TotodileWaterPulse.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath("TotodileAttack.png");
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = Pokemon.Enums.TOTODILE_BLUE;
+
+    private static final int COST = 0;
+    private static final int DAMAGE = 3;
+    private static final int BASE_MAGIC_NUMBER = 4;
+    private static final int UPGRADE_MAGIC_NUMBER = 1;
+
+
+    // /STAT DECLARATION/
+
+    public TotodileWaterPulse() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+        baseMagicNumber = BASE_MAGIC_NUMBER;
+        magicNumber = baseMagicNumber;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        int numberOfHits = p.drawPile.size() > magicNumber? magicNumber: p.drawPile.size();
+        addToBot(new DiscardTopOfDrawPileAction(magicNumber));
+        for(int i=0;i<numberOfHits;i++)
+            addToBot( new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AttackEffect.SMASH));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
+            initializeDescription();
+        }
+    }
+}
