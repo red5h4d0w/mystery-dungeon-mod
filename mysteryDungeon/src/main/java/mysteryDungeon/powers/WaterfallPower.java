2de1cfda12c2bc8c74d1768c53196c3dc124d0e3
Ivel54 <abudjo55@gmail.com>
2023-01-04 04:11:23 -0500

    Waterfall

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/powers/WaterfallPower.java
@@ -0,0 +1,77 @@
+package mysteryDungeon.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonPower;
+import mysteryDungeon.util.TextureLoader;
+
+import static mysteryDungeon.MysteryDungeon.makePowerPath;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.EmptyDeckShuffleAction;
+import com.megacrit.cardcrawl.actions.common.ShuffleAction;
+import com.megacrit.cardcrawl.actions.utility.ScryAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+
+//Gain 1 dex for the turn for each card played.
+
+public class WaterfallPower extends PokemonPower implements CloneablePowerInterface {
+    public AbstractCreature source;
+    public int counter = 0;
+
+    public static final String POWER_ID = MysteryDungeon.makeID(WaterfallPower.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath(WaterfallPower.class.getSimpleName()+"84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath(WaterfallPower.class.getSimpleName()+"32.png"));
+
+    public WaterfallPower(final AbstractCreature owner, final int amount) {
+        name = NAME;
+        ID = POWER_ID;
+
+        this.owner = owner;
+        this.amount = amount;
+
+        type = PowerType.BUFF;
+
+        // We load those txtures here.
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+    
+    @Override
+    public void atEndOfTurn(boolean isPlayer) {
+        if (AbstractDungeon.player.discardPile.size() > 0) {
+            addToBot(new EmptyDeckShuffleAction());
+            addToBot(new ShuffleAction(AbstractDungeon.player.drawPile, false));
+        }
+    }
+
+    @Override
+    public void atStartOfTurn()
+    {
+        addToBot(new ScryAction(amount));
+    }
+    @Override
+    public AbstractPower makeCopy() {
+        return new WaterfallPower(owner, amount);
+    }
+
+    @Override
+    public void updateDescription() {
+            description = DESCRIPTIONS[0];
+        }
+}
