39d996069736581eb2db6ac09e43a13a37e0c969
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2023-01-04 20:39:59 -0500

    Create AvalanchePower.java

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/powers/AvalanchePower.java
@@ -0,0 +1,81 @@
+package mysteryDungeon.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonPower;
+import mysteryDungeon.util.TextureLoader;
+
+import static mysteryDungeon.MysteryDungeon.makePowerPath;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.DamageRandomEnemyAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.orbs.AbstractOrb;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+
+//Gain 1 dex for the turn for each card played.
+
+public class AvalanchePower extends PokemonPower implements CloneablePowerInterface {
+    public AbstractCreature source;
+
+    public static final String POWER_ID = MysteryDungeon.makeID(AvalanchePower.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath(AvalanchePower.class.getSimpleName()+"84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath(AvalanchePower.class.getSimpleName()+"32.png"));
+
+    public AvalanchePower(final AbstractCreature owner, final int amount) {
+        name = NAME;
+        ID = POWER_ID;
+
+        this.owner = owner;
+        this.amount = amount;
+
+        type = PowerType.BUFF;
+
+        // We load those txtures here.
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    @Override
+    public void onEvokeOrb(AbstractOrb orb) {
+        int blockAmount = AbstractDungeon.player.currentBlock;
+        if(blockAmount!=0) {
+            addToBot(new DamageRandomEnemyAction(new DamageInfo(AbstractDungeon.player, blockAmount, DamageType.THORNS), AttackEffect.BLUNT_LIGHT));
+        }
+        super.onEvokeOrb(orb);
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new AvalanchePower(owner, amount);
+    }
+
+    @Override
+    public void updateDescription() {
+        if(amount == 1)
+        {
+            description = DESCRIPTIONS[0];
+        } 
+        else
+        {
+            description = String.format(DESCRIPTIONS[1], amount);
+        }
+    }
+}
