72e20284808cf1722380a730e8594d8cd975e6ec
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2023-01-20 22:49:42 -0500

    art56

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Chikorita/ChikoritaExplorerForm.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Chikorita/ChikoritaExplorerForm.java
@@ -19,7 +19,7 @@ public class ChikoritaExplorerForm extends PokemonCard {
 
     public static final String ID = MysteryDungeon.makeID(ChikoritaExplorerForm.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("ChikoritaPower.png");
+    public static final String IMG = makeCardPath(ChikoritaExplorerForm.class.getSimpleName()+".png");
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
