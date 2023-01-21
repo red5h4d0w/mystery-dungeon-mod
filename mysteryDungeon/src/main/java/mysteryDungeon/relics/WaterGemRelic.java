0c553075783647694c6f1940d53859480517a174
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2023-01-21 13:58:22 -0500

    Create WaterGemRelic.java

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/relics/WaterGemRelic.java
@@ -0,0 +1,52 @@
+package mysteryDungeon.relics;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonRelic;
+import mysteryDungeon.pokemons.Totodile;
+import mysteryDungeon.util.TextureLoader;
+
+import static mysteryDungeon.MysteryDungeon.makeRelicOutlinePath;
+import static mysteryDungeon.MysteryDungeon.makeRelicPath;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.RelicStrings;
+
+public class WaterGemRelic extends PokemonRelic { 
+
+    // ID, images, text.
+    public static final String ID = MysteryDungeon.makeID(WaterGemRelic.class.getSimpleName());
+
+    private static final RelicStrings relicStrings = CardCrawlGame.languagePack.getRelicStrings(ID);
+    public static final String NAME = relicStrings.NAME;
+    public static final String[] DESCRIPTIONS = relicStrings.DESCRIPTIONS;
+    private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("wave-incense.png"));
+    private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("wave-incense.png"));
+
+    public WaterGemRelic() {
+        super(ID, IMG, OUTLINE, RelicTier.COMMON, LandingSound.CLINK);
+        cardColor = Totodile.CARD_COLOR;
+    }
+
+    @Override
+    public void onAttack(DamageInfo info, int damageAmount, AbstractCreature target) {
+        super.onAttack(info, damageAmount, target);
+        if(info.owner != AbstractDungeon.player)  {
+            return;
+        }
+        if(damageAmount > 20) {
+            addToBot(new DamageAllEnemiesAction(AbstractDungeon.player, DamageInfo.createDamageMatrix(5), DamageType.THORNS, AttackEffect.FIRE));
+        }
+    }
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+
+}
