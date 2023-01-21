09edd85789d121f35daa8677268375cdb1548fdd
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2023-01-21 13:31:52 -0500

    Create SplashPlateRelic.java

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/relics/SplashPlateRelic.java
@@ -0,0 +1,53 @@
+package mysteryDungeon.relics;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.RelicStrings;
+import com.megacrit.cardcrawl.orbs.AbstractOrb;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonRelic;
+import mysteryDungeon.pokemons.Meowth;
+import mysteryDungeon.util.TextureLoader;
+
+import static mysteryDungeon.MysteryDungeon.makeRelicOutlinePath;
+import static mysteryDungeon.MysteryDungeon.makeRelicPath;
+
+public class SplashPlateRelic extends PokemonRelic {
+    public static final String ID = MysteryDungeon.makeID(SplashPlateRelic.class.getSimpleName());
+
+    private static final RelicStrings relicStrings = CardCrawlGame.languagePack.getRelicStrings(ID);
+    public static final String NAME = relicStrings.NAME;
+    public static final String[] DESCRIPTIONS = relicStrings.DESCRIPTIONS;
+    private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("big-root.png"));
+    private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("big-root.png"));
+
+    private boolean used;
+
+    public SplashPlateRelic() {
+        super(ID, IMG, OUTLINE, RelicTier.BOSS, LandingSound.CLINK);
+        cardColor = Meowth.CARD_COLOR;
+    }
+
+    @Override
+    public void onEvokeOrb(AbstractOrb ammo) {
+        super.onEvokeOrb(ammo);
+        if(!used) {
+            addToBot(new GainEnergyAction(1));
+            used = true;
+        }
+    }
+
+    @Override
+    public void atTurnStart() {
+        super.atTurnStart();
+        used = false;
+    }
+
+    // Description
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+}
