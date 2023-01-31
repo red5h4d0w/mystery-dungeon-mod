1922b93b988d3db9af43751a26ec84fe24ebb9de
Ivel54 <71346308+Ivel54@users.noreply.github.com>
2023-01-30 20:59:45 -0500

    Better

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthPsychUp.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthPsychUp.java
@@ -19,7 +19,7 @@ public class MeowthPsychUp extends PokemonCard {
 
     public static final String ID = MysteryDungeon.makeID(MeowthPsychUp.class);
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("MeowthPower");
+    public static final String IMG = makeCardPath(MeowthPsychUp.class);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 
