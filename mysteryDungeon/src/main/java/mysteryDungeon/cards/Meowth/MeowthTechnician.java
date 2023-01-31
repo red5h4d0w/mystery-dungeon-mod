b57483409c4a163bc184ea7a84557f7a20f08f3d
Ivel54 <71346308+Ivel54@users.noreply.github.com>
2023-01-30 20:59:20 -0500

    Should be fixed

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthTechnician.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Meowth/MeowthTechnician.java
@@ -21,7 +21,7 @@ public class MeowthTechnician extends PokemonCard {
 
     public static final String ID = MysteryDungeon.makeID(MeowthTechnician.class);
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("MeowthPower");
+    public static final String IMG = makeCardPath(MeowthTechnician.class);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
