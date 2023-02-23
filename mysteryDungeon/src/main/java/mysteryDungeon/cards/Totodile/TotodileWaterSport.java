60a244498439449d3fdb429bb42bfece7ab8e279
Ivel54 <71346308+Ivel54@users.noreply.github.com>
2023-02-23 16:52:05 -0500

    Water sport buff

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Totodile/TotodileWaterSport.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Totodile/TotodileWaterSport.java
@@ -35,10 +35,10 @@ public class TotodileWaterSport extends PokemonCard {
     public static final CardColor COLOR = Pokemon.Enums.TOTODILE_BLUE;
 
     private static final int COST = 1;
-    private static final int BASE_MAGIC_NUMBER = 1;
+    private static final int BASE_MAGIC_NUMBER = 2;
     private static final int UPGRADE_MAGIC_NUMBER = 1;
     private static final int BASE_SECOND_MAGIC_NUMBER = 6;
-    private static final int UPGRADE_SECOND_MAGIC_NUMBER = 2;
+    private static final int UPGRADE_SECOND_MAGIC_NUMBER = 3;
 
     // /STAT DECLARATION
 
