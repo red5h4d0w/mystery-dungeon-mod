d32c3e327f6a786246403258adaef562d69e4226
Ivel54 <71346308+Ivel54@users.noreply.github.com>
2023-02-23 16:53:40 -0500

    Crucnch balance change

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Totodile/TotodileCrunch.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Totodile/TotodileCrunch.java
@@ -35,9 +35,9 @@ public class TotodileCrunch extends PokemonCard {
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = Pokemon.Enums.TOTODILE_BLUE;
 
-    private static final int COST = 2;
-    private static final int DAMAGE = 10;
-    private static final int UPGRADE_PLUS_DMG = 4;
+    private static final int COST = 1;
+    private static final int DAMAGE = 7;
+    private static final int UPGRADE_PLUS_DMG = 3;
     private static final int BASE_MAGIC_NUMBER = 1;
 
 
