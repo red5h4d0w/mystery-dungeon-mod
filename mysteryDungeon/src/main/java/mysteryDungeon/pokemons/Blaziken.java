c57c80f571eb6c2ec5555086b6f6b6294c6bb5a6
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2023-10-06 00:25:48 -0400

    forgot to save blaziken

--- mysteryDungeon/src/main/java/mysteryDungeon/pokemons/Blaziken.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/pokemons/Blaziken.java
@@ -1,25 +1,16 @@
 package mysteryDungeon.pokemons;
 
 import com.badlogic.gdx.graphics.Color;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.AbstractCard.CardColor;
 
-import mysteryDungeon.cards.Bulbasaur.BulbasaurDefend;
-import mysteryDungeon.cards.Bulbasaur.BulbasaurLeechSeed;
-import mysteryDungeon.cards.Bulbasaur.BulbasaurTackle;
 import mysteryDungeon.characters.Pokemon;
 
-public class Torchic extends AbstractPokemon {
+public class Blaziken extends AbstractPokemon {
 
-    public static int MAX_HP = 35;
-    public static int ORB_SLOTS = 1;
-    public static AbstractCard[] STARTING_DECK = new AbstractCard[] {
-        new BulbasaurTackle(), new BulbasaurTackle(), new BulbasaurDefend(), new BulbasaurDefend(), new BulbasaurLeechSeed()
-    };
     public static Color COLOR = Color.RED.cpy();
-    public static CardColor CARD_COLOR = Pokemon.Enums.NOT_IMPLEMENTED_COLOR;
+    public static CardColor CARD_COLOR = Pokemon.Enums.TORCHIC_RED;
 
-    public Torchic(){
-        super(MAX_HP, ORB_SLOTS, STARTING_DECK, COLOR, CARD_COLOR);
+    public Blaziken(){
+        super(COLOR, CARD_COLOR);
     }
 }
