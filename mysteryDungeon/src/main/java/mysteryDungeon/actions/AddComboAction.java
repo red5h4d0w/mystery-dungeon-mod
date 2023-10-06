1ccb1d38509839352a6ea2fc6a7e74659a17d777
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2023-10-06 09:25:23 -0400

    add action and example power

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/actions/AddComboAction.java
@@ -0,0 +1,20 @@
+package mysteryDungeon.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+
+import mysteryDungeon.characters.Pokemon;
+import mysteryDungeon.util.TorchicComboManager.Combo;
+
+public class AddComboAction extends AbstractGameAction {
+    
+    private Combo combo;
+
+    public AddComboAction(Combo combo) {
+        this.combo = combo;
+    }
+
+    public void update() {
+        Pokemon.comboManager.addCombo(combo);
+    }
+
+}
