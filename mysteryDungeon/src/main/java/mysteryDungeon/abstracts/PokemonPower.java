a627535febfb84eabb603acbf7ece5c9bba53011
Ivel54 <71346308+Ivel54@users.noreply.github.com>
2023-10-13 09:47:34 -0400

    Fire spin trigger

--- mysteryDungeon/src/main/java/mysteryDungeon/abstracts/PokemonPower.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/abstracts/PokemonPower.java
@@ -23,4 +23,8 @@ public class PokemonPower extends AbstractPower {
     public void updateDescription() {
         this.description = DESCRIPTIONS[0];
     }
+
+    public void triggerFireSpin() {
+        
+    }
 }
---
+++ mysteryDungeon/src/main/java/mysteryDungeon/actions/TriggerFireSpinAction.java
@@ -0,0 +1,25 @@
+package mysteryDungeon.actions;
+
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import mysteryDungeon.abstracts.PokemonPower;
+
+public class TriggerFireSpinAction extends AbstractGameAction {
+  AbstractCard card;
+  
+  public TriggerFireSpinAction(AbstractCard callingCard) {
+    this.card = callingCard;
+  }
+  
+  public void update() {
+    for (AbstractMonster mo : (AbstractDungeon.getCurrRoom()).monsters.monsters) {
+      for (PokemonPower p : mo.powers.stream().filter(p->p instanceof PokemonPower).toArray(PokemonPower[]::new))
+        p.triggerFireSpin(); 
+    } 
+    this.isDone = true;
+  }
+}
