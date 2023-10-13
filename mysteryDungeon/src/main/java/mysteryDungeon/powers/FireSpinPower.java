7d8ddd63327d3352aa39cc3a8574f8acf5f90fea
Ivel54 <71346308+Ivel54@users.noreply.github.com>
2023-10-13 09:47:34 -0400

    Fire spin trigger

--- mysteryDungeon/src/main/java/mysteryDungeon/powers/FireSpinPower.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/powers/FireSpinPower.java
@@ -7,10 +7,8 @@ import mysteryDungeon.util.TextureLoader;
 
 import static mysteryDungeon.MysteryDungeon.makePowerPath;
 
-
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.OnReceivePowerPower;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.LoseHPAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
@@ -18,22 +16,22 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
-
 //Gain 1 dex for the turn for each card played.
 
-public class FireSpinPower extends PokemonPower implements CloneablePowerInterface, OnReceivePowerPower {
+public class FireSpinPower extends PokemonPower implements CloneablePowerInterface {
     public AbstractCreature source;
 
     public static final String POWER_ID = MysteryDungeon.makeID(FireSpinPower.class);
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
-    
 
-    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
-    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
-    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath(FireSpinPower.class)+"84.png");
-    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath(FireSpinPower.class)+"32.png");
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84
+    // image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if
+    // you accidentally put a non-existent file.
+    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath(FireSpinPower.class) + "84.png");
+    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath(FireSpinPower.class) + "32.png");
 
     public FireSpinPower(final AbstractCreature owner, final int amount) {
         name = NAME;
@@ -50,20 +48,10 @@ public class FireSpinPower extends PokemonPower implements CloneablePowerInterfa
 
         updateDescription();
     }
-        public void onInitialApplication()
-    {
-        addToBot((AbstractGameAction)new LoseHPAction(this.owner, null, this.amount, AbstractGameAction.AttackEffect.FIRE));
-        
-    }
 
-    public boolean onReceivePower(AbstractPower power, AbstractCreature target, AbstractCreature source)
-    {
-         if(power.ID == FireSpinPower.POWER_ID)
-         addToBot((AbstractGameAction)new LoseHPAction(this.owner, null, this.amount, AbstractGameAction.AttackEffect.FIRE));
-            updateDescription();
-        return true;
+    public void triggerFireSpin() {
+        addToBot(new LoseHPAction(this.owner, null, this.amount, AbstractGameAction.AttackEffect.FIRE));
     }
-
 
     @Override
     public AbstractPower makeCopy() {
@@ -73,5 +61,5 @@ public class FireSpinPower extends PokemonPower implements CloneablePowerInterfa
     @Override
     public void updateDescription() {
         this.description = powerStrings.DESCRIPTIONS[0] + this.amount + powerStrings.DESCRIPTIONS[1];
-      }
+    }
 }
