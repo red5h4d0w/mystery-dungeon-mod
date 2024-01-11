9fa7e3bccd1e1da9c8654880d906121683347786
Ivel54 <71346308+Ivel54@users.noreply.github.com>
2024-01-11 14:00:26 -0500

    3 new cards for torchic facade est fini mais pas bulk up et low sweep

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Torchic/TorchicFacade.java
@@ -0,0 +1,66 @@
+package mysteryDungeon.cards.Torchic;
+
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonCard;
+import mysteryDungeon.actions.AddComboAction;
+import mysteryDungeon.actions.SimpleAction;
+import mysteryDungeon.characters.Pokemon;
+import mysteryDungeon.util.TorchicComboManager.Combo;
+import mysteryDungeon.util.TorchicComboManager.Move;
+
+public class TorchicFacade extends PokemonCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(TorchicFacade.class);
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath(TorchicFacade.class);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = Pokemon.Enums.TORCHIC_RED;
+
+    private static final int COST = 1;
+    private static final int UPGRADE_COST = 0;
+
+    // /STAT DECLARATION/
+
+    public TorchicFacade() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new AddComboAction(new Combo(new SimpleAction(() -> {
+            // Stuff to do in the finisher
+            // IMPORTANT: addToTop in Finisher, because finisher is itself an action 
+            addToTop(new GainBlockAction(p, 7));
+        }), Move.SKILL, Move.SKILL)));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADE_COST);
+            initializeDescription();
+        }
+    }
+}
---
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Torchic/TorchicLowSweep.java
@@ -0,0 +1,73 @@
+package mysteryDungeon.cards.Torchic;
+
+import static mysteryDungeon.MysteryDungeon.makeCardPath;
+
+import com.evacipated.cardcrawl.mod.stslib.actions.common.MoveCardsAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.unique.DiscardPileToTopOfDeckAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonCard;
+import mysteryDungeon.characters.Pokemon;
+
+public class TorchicLowSweep extends PokemonCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = MysteryDungeon.makeID(TorchicLowSweep.class);
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG = makeCardPath(TorchicLowSweep.class);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = Pokemon.Enums.TORCHIC_RED;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 4;
+    private static final int UPGRADE_PLUS_DMG = 2;
+
+    // /STAT DECLARATION/
+
+    public TorchicLowSweep() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SMASH));
+        addToBot((AbstractGameAction)new DiscardPileToTopOfDeckAction((AbstractCreature)p));
+       if (!AbstractDungeon.actionManager.cardsPlayedThisCombat.isEmpty() && ((AbstractCard)AbstractDungeon.actionManager.cardChosen
+          .get(AbstractDungeon.actionManager.cardChosen
+            .size() - 1)).type == AbstractCard.CardType.ATTACK)
+            addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SMASH));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            initializeDescription();
+        }
+    }
+}
