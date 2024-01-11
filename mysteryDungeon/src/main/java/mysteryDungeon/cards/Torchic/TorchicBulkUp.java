0ca813b684359fea4b305c963804acbb7f875fe2
Ivel54 <71346308+Ivel54@users.noreply.github.com>
2024-01-11 15:06:24 -0500

    bulk up almost complete

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Torchic/TorchicBulkUp.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Torchic/TorchicBulkUp.java
@@ -2,19 +2,21 @@ package mysteryDungeon.cards.Torchic;
 
 import static mysteryDungeon.MysteryDungeon.makeCardPath;
 
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.actions.common.PlayTopCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import mysteryDungeon.MysteryDungeon;
 import mysteryDungeon.abstracts.PokemonCard;
-import mysteryDungeon.actions.AddComboAction;
-import mysteryDungeon.actions.SimpleAction;
 import mysteryDungeon.characters.Pokemon;
-import mysteryDungeon.util.TorchicComboManager.Combo;
-import mysteryDungeon.util.TorchicComboManager.Move;
 
 public class TorchicBulkUp extends PokemonCard {
 
@@ -47,12 +49,23 @@ public class TorchicBulkUp extends PokemonCard {
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new AddComboAction(new Combo(new SimpleAction(() -> {
-            // Stuff to do in the finisher
-            // IMPORTANT: addToTop in Finisher, because finisher is itself an action 
-            addToTop(new GainBlockAction(p, 7));
-        }), Move.SKILL, Move.SKILL)));
-    }
+        AbstractCard[] possibleCards = CardLibrary.cards.values().stream()
+                .filter(c -> c instanceof PokemonCard)
+                .filter(c -> c.color == ((Pokemon)AbstractDungeon.player).adventurer.cardColor)
+                .filter(c -> c.type == CardType.POWER)
+                .filter(c -> !c.tags.contains(CardTags.STARTER_DEFEND))
+                .filter(c -> !c.tags.contains(CardTags.STARTER_STRIKE))
+                .toArray(AbstractCard[]::new);
+            AbstractCard c = possibleCards[(int)AbstractDungeon.cardRng.random(possibleCards.length-1)];
+            if (c.cost > 0) {
+                c.cost = 0;
+                c.costForTurn = 0;
+                c.isCostModified = true;
+              } 
+        addToBot(new MakeTempCardInDrawPileAction(c, 1, false, false, false));
+        addToBot((AbstractGameAction)new PlayTopCardAction(
+          (AbstractCreature)(AbstractDungeon.getCurrRoom()).monsters.getRandomMonster(null, true, AbstractDungeon.cardRandomRng), true));
+}
 
     // Upgraded stats.
     @Override
