234952cbcade12339443bd2c69633f55a7725114
Ivel54 <abudjo55@gmail.com>
2024-01-27 16:09:08 -0500

    low sweep update

--- mysteryDungeon/src/main/java/mysteryDungeon/cards/Torchic/TorchicLowSweep.java
+++ mysteryDungeon/src/main/java/mysteryDungeon/cards/Torchic/TorchicLowSweep.java
@@ -25,7 +25,7 @@ public class TorchicLowSweep extends PokemonCard {
 
     public static final String ID = MysteryDungeon.makeID(TorchicLowSweep.class);
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("TorchiAttack");
+    public static final String IMG = makeCardPath("TorchicAttack");
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 
